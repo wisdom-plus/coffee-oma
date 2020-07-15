e0501c8ffa72e8b27bdb9f5fcb6535a8c2d464a5
tomoron <tomoron326@outlook.jp>
2020-07-15 12:47:36 +0000

    chore:devise_rails_db:migrate

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 end
