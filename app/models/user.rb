2350e2584b3fbfffbd14e43b5122e3c7fa3a2368
tomoron <tomoron326@outlook.jp>
2020-07-21 00:59:03 +0000

    fest:registrations_update_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
+
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confitmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    result =update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
