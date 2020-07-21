87147b23fc5c5dd88b8165efef834a93e170eec8
tomoron <tomoron326@outlook.jp>
2020-07-21 09:53:22 +0000

    test:request_password

---
+++ spec/requests/passwords_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "Passwords", type: :request do
+  let(:user) {create(:user)}
+
+  describe "GET users/password/new" do
+    it "request seccess" do
+      get new_user_password_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET users/password/edit" do
+    before do
+      @raw, enc = Devise.token_generator.generate(User, :reset_password_token)
+
+      user.reset_password_token   = enc
+      user.reset_password_sent_at = (Time.now - 4.hours)
+      user.save(validate: false)
+    end
+
+    it "rquest seccess" do
+      get edit_user_password_path(@resources, reset_password_token: @raw)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST users/password" do
+    it "request seccess" do
+      post user_password_path, params: {user: {email: user.email}}
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "PATCH users/password" do
+    before do
+      @raw, enc = Devise.token_generator.generate(User, :reset_password_token)
+
+      user.reset_password_token   = enc
+      user.reset_password_sent_at = (Time.now - 4.hours)
+      user.save(validate: false)
+    end
+
+    it "requests seccess" do
+      patch user_password_path, params: { user: {reset_password_token: @raw, password: "password", password_confirmation: "password"}}
+      expect(response).to have_http_status(302)
+    end
+  end
+end
