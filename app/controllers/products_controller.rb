99d291563856b7fe76656aa5f7f0f4d9ab62d27a
伯之中井 <hakunonakai@tomoMac.local>
2020-07-27 20:48:58 +0900

    update:routes_product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,34 @@
 class ProductsController < ApplicationController
   def search
     if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword],imageFlag: 1)
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword],imageFlag: 1, elements: "itemName")
     end
+    redirect_to action: "index"
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+  end
+
+  def index
+
+  end
+
+  def update
+
+  end
+
+  def edit
+  end
+
+  def show
+
   end
 end
