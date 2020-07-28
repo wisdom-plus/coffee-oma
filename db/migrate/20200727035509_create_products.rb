aa9c96597c9222b70be9fecdbb2dc1bd57458684
tomoron <tomoron326@outlook.jp>
2020-07-28 21:42:50 +0900

    Add:product_create

--- db/migrate/20200727035509_create_products.rb
+++ db/migrate/20200727035509_create_products.rb
@@ -3,9 +3,10 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string :itemname
       t.string :catchcopy
+      t.integer :itemprice
       t.text :itemcaption
       t.text :itemurl
-      t.text :mediumlmageurls
+      t.text :imageurl
       t.string :shopname
 
       t.timestamps
