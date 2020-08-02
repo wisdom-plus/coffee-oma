be88ab938fde660c8597d33ebdb4082b53453565
tomoron <tomoron326@outlook.jp>
2020-08-02 19:37:49 +0900

    test:product_render_page

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature "Products", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.describe "Products", type: :feature do
+  let(:product) { create(:product)}
+
+  describe 'product' do
+    describe 'new' do
+      it 'render page' do
+        visit new_product_path
+        expect(page).to have_current_path new_product_path
+      end
+    end
+
+    describe 'create' do
+      context 'redirect page' do
+
+      end
+    end
+
+    describe 'index' do
+      it 'render page' do
+        visit products_path
+        expect(page).to have_current_path products_path
+      end
+    end
+
+    describe 'show' do
+      before do
+        product
+      end
+      it 'render page' do
+        visit product_path(product.id)
+        expect(page).to have_current_path product_path(product.id)
+        end
+    end
+  end
 end
