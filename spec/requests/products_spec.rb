ee79d70bd1adccd4fce95c7ffabba6d91c06731d
tomoron <tomoron326@outlook.jp>
2020-08-03 12:39:22 +0900

    test:request_product

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,9 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
+  let(:product) { create(:product)}
+  let(:product_params) { attributes_for(:product)}
+
   describe "GET /products" do
-    it "works! (now write some real specs)" do
-      get products_index_path
+    it "request seccees" do
+      get products_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /products/new' do
+    it "request seccees" do
+      get new_product_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST /products' do
+    it 'request seccees' do
+      post products_path, params: {product: product_params}
+      expect(response).to have_http_status(302)
+    end
+
+    it 'prodcut created' do
+      expect do
+        post products_path, params: {product: product_params}
+      end.to change(Product, :count).by 1
+    end
+  end
+
+  describe 'GET /products/:id' do
+    it "request seccees" do
+      get product_path(product.id)
       expect(response).to have_http_status(200)
     end
   end
