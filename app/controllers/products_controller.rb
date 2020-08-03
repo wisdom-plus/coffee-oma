d440ff3eb0c64bd11d4a34a19ef9786d8f2077da
tomoron <tomoron326@outlook.jp>
2020-08-03 12:41:45 +0900

    rubocop

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-
   def new
     if params[:keyword]
       @items = rakuten_search(params[:keyword])
@@ -23,11 +22,11 @@ class ProductsController < ApplicationController
 
   private
 
-  def rakuten_search(search_keyword)
-    RakutenWebService::Ichiba::Item.search(keyword: search_keyword,imageFlag: 1)
-  end
+    def rakuten_search(search_keyword)
+      RakutenWebService::Ichiba::Item.search(keyword: search_keyword, imageFlag: 1)
+    end
 
-  def product_params
-    params.require(:product).permit(:itemname, :itemprice, :shopname, :catchcopy, :imageurl,:itemurl, :itemcaption)
-  end
+    def product_params
+      params.require(:product).permit(:itemname, :itemprice, :shopname, :catchcopy, :imageurl, :itemurl, :itemcaption)
+    end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "Products", type: :request do
-  let(:product) { create(:product)}
-  let(:product_params) { attributes_for(:product)}
+RSpec.describe 'Products', type: :request do
+  let(:product) { create(:product) }
+  let(:product_params) { attributes_for(:product) }
 
-  describe "GET /products" do
-    it "request seccees" do
+  describe 'GET /products' do
+    it 'request seccees' do
       get products_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'GET /products/new' do
-    it "request seccees" do
+    it 'request seccees' do
       get new_product_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe 'POST /products' do
     it 'request seccees' do
-      post products_path, params: {product: product_params}
-      expect(response).to have_http_status(302)
+      post products_path, params: { product: product_params }
+      expect(response).to have_http_status(:found)
     end
 
     it 'prodcut created' do
       expect do
-        post products_path, params: {product: product_params}
+        post products_path, params: { product: product_params }
       end.to change(Product, :count).by 1
     end
   end
 
   describe 'GET /products/:id' do
-    it "request seccees" do
+    it 'request seccees' do
       get product_path(product.id)
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
