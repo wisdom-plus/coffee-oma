5b32f6dc2d811752de0149a91f8d4c40796b5297
tomoron <tomoron326@outlook.jp>
2020-08-05 22:10:26 +0900

    review_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,17 @@
 class ReviewsController < ApplicationController
   def create
-
+    review = Review.new(review_params, user_id: current_user.id)
+    review.save
+    redirect_to "/products/#{review_params[:product_id]}"
   end
 
   def index
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :product_id)
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,20 +2,20 @@
 #
 # Table name: reviews
 #
-#  id            :bigint           not null, primary key
-#  content       :text(65535)
-#  title         :string(255)
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  product_id_id :bigint
-#  user_id_id    :bigint
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  product_id :bigint
+#  user_id    :bigint
 #
 # Indexes
 #
-#  index_reviews_on_product_id_id  (product_id_id)
-#  index_reviews_on_user_id_id     (user_id_id)
+#  index_reviews_on_product_id  (product_id)
+#  index_reviews_on_user_id     (user_id)
 #
 class Review < ApplicationRecord
   belongs_to :user
-  belogns_to :product
+  belongs_to :product
 end
