095725b39a3273d8aa4633a5588f0b4dc7aef5a5
tomoron <tomoron326@outlook.jp>
2020-08-09 21:54:22 +0900

    rubocop

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "Reviews", type: :feature do
-  let(:user) {create(:user)}
-  let(:product) { create(:product)}
-  let(:review) { create(:review, user: user, product: product)}
+RSpec.describe 'Reviews', type: :feature do
+  let(:user) { create(:user) }
+  let(:product) { create(:product) }
+  let(:review) { create(:review, user: user, product: product) }
 
   describe 'review' do
     describe 'index' do
@@ -21,14 +21,16 @@ RSpec.feature "Reviews", type: :feature do
         login(user, user.email, user.password)
         visit product_path(product.id)
       end
+
       context 'seccess' do
         before do
-          fill_in "タイトル",	with: "コーヒー器具のタイトル"
-          fill_in "レビュー内容",	with: "コーヒー器具の内容"
-          click_button "  submit  "
+          fill_in 'タイトル',	with: 'コーヒー器具のタイトル'
+          fill_in 'レビュー内容',	with: 'コーヒー器具の内容'
+          click_button '  submit  '
         end
+
         it 'redirect page' do
-          expect(current_path).to eq product_path(product.id)
+          expect(page).to have_current_path product_path(product.id), ignore_query: true
         end
 
         it 'displayed review' do
@@ -38,16 +40,16 @@ RSpec.feature "Reviews", type: :feature do
 
       context 'failure' do
         it 'black title' do
-          fill_in "タイトル",	with: ""
-          fill_in "レビュー内容",	with: "コーヒー器具の内容"
-          click_button "  submit  "
+          fill_in 'タイトル',	with: ''
+          fill_in 'レビュー内容',	with: 'コーヒー器具の内容'
+          click_button '  submit  '
           expect(page).to have_no_content 'コーヒー器具の内容'
         end
 
         it 'black content' do
-          fill_in "タイトル",	with: "コーヒー器具のタイトル"
-          fill_in "レビュー内容",	with: ""
-          click_button "  submit  "
+          fill_in 'タイトル',	with: 'コーヒー器具のタイトル'
+          fill_in 'レビュー内容',	with: ''
+          click_button '  submit  '
           expect(page).to have_no_content 'コーヒー器具のタイトル'
         end
       end
