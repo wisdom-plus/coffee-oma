cc8acfb0950b1385358c5c5b4cb6987a903cb593
tomoron <tomoron326@outlook.jp>
2020-08-11 16:51:03 +0900

    test:request_like

--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -15,6 +15,7 @@
 #
 FactoryBot.define do
   factory :like do
-    
+    user
+    product
   end
 end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,18 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
+  let(:user) { create(:user)}
+  let(:product) { create(:product)}
+  let(:like) { create(:like, user: user, product: product)}
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/likes/create"
-      expect(response).to have_http_status(:success)
+  describe "POST create" do
+    before do
+      user.confirm
+      sign_in user
+    end
+
+    it "request success" do
+      post likes_path, params: {product_id: product.id}
+      expect(response).to have_http_status(302)
+    end
+
+    it 'created like success' do
+      expect do
+        post likes_path, params: {product_id: product.id}
+      end.to change(Like, :count).by 1
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/likes/destroy"
-      expect(response).to have_http_status(:success)
+  describe "DELETE destroy" do
+    before do
+      user.confirm
+      sign_in user
+      like
+    end
+
+    it "request success" do
+      delete like_path(like.id)
+      expect(response).to have_http_status(302)
+    end
+
+    it 'destroy like success' do
+      expect do
+        delete like_path(like.id)
+      end.to change(Like, :count).by -1
     end
   end
 
