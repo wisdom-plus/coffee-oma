fffdbef29cbe1874514b02fbb03e12fb4004ffd4
tomoron <tomoron326@outlook.jp>
2020-08-12 20:18:23 +0900

    fest:follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,11 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers,through: :reverse_of_relationships, source: :user
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :timeoutable
 
