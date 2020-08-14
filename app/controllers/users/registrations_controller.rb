379a755a53e1646061759356577629933f57cab3
tomoron <tomoron326@outlook.jp>
2020-08-14 22:09:24 +0900

    test:users_show

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -31,7 +31,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def show
     @user = User.find(params[:id])
-    @follow = Relationship.find_by(user_id: current_user.id, follow_id: @user.id)
+    @follow = Relationship.find_by(user_id: current_user.id, follow_id: @user.id) if signed_in?
   end
 
   # GET /resource/cancel
