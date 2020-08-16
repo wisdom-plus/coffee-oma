87cdc6ea62d1ce1eab711b3da11a5613531a2dac
tomoron <tomoron326@outlook.jp>
2020-08-16 21:45:07 +0900

    chore:icon_uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_043923) do
+ActiveRecord::Schema.define(version: 2020_08_16_122656) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_043923) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.text "profile"
+    t.string "icon"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
