9cd4a21d0047185884fa9ece9d12b1d6776a7a8b
tomoron <tomoron326@outlook.jp>
2020-08-17 18:54:50 +0900

    style:users_show_icon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username :icon])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username :icon])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username , :icon])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username , :icon])
     end
 end
