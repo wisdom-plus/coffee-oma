9f30a9efe4fbdba0b5970d3dce9c5e64785c5dac
tomoron <tomoron326@outlook.jp>
2020-08-18 15:04:04 +0900

    stlye:user_edit_icon

--- app/javascript/custom/users.js
+++ app/javascript/custom/users.js
@@ -1,15 +1,17 @@
 $(function () {
   $('.close').click(function () {
     $('.flash-message').closest('.flash-message').transition('fade');
-  })
+  });
   $('.sticky')
     .sticky({
       context: '#example1'
-    })
-    ;
+    });
   $('.menu .item').tab();
   $(document).on('turbolinks:load', function () {
     $('.ui.accordion').accordion();
   });
   $('.ui.accordion').accordion();
+  $('#dimmer-button').on('click', function () {
+    $('#dimmer').dimmer('toggle');
+  })
 });
