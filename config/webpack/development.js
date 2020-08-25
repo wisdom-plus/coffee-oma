d2635a099532112e4c2d1de9f0472e881aec5e57
tomoron <tomoron326@outlook.jp>
2020-08-25 13:39:08 +0900

    add:infinite-scroll

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,11 +2,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+
 module.exports = environment.toWebpackConfig()
