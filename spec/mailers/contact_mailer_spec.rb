9f8bb5d1479c00ad66c08b0295c55c5ea3f445af
tomoron <tomoron326@outlook.jp>
2020-08-29 14:41:44 +0900

    test:contact_mailer]

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,5 +1,35 @@
 require "rails_helper"
 
 RSpec.describe ContactMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:contact) {create(:contact)}
+
+  describe 'contact' do
+    context 'contact_mail' do
+      subject(:mail) do
+        ContactMailer.contact_mail(contact).deliver_now
+        mail = ActionMailer::Base.deliveries.last
+      end
+
+      it 'send mail' do
+        expect(mail.from.first).to eq('system@example.com')
+        expect(mail.to.first).to eq(ENV['MY_MAILADRESS'])
+        expect(mail.subject).to eq('お問い合わせ通知')
+        expect(mail.body).to match('これは問合せメールの本文が入ります')
+      end
+    end
+
+    context 'reply_mail' do
+      subject(:mail) do
+        ContactMailer.reply_mail(contact).deliver_now
+        mail = ActionMailer::Base.deliveries.last
+      end
+
+      it 'send mail' do
+        expect(mail.from.first).to eq('system@example.com')
+        expect(mail.to.first).to eq('test@example.com')
+        expect(mail.subject).to eq('お問い合わせありがとうございます')
+        expect(mail.body).to match('これは問合せメールの本文が入ります')
+      end
+    end
+  end
 end
