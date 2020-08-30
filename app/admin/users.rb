2c8ed4076136150d8d09bf6689d1b00560206083
tomoron <tomoron326@outlook.jp>
2020-08-30 21:23:39 +0900

    styly:product_show

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,38 @@
 ActiveAdmin.register User do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :username, :profile, :icon
+  # permit_params :email,
+  #               :encrypted_password,
+  #               :reset_password_token,
+  #               :reset_password_sent_at,
+  #               :remember_created_at,
+  #               :confirmation_token,
+  #               :confirmed_at,
+  #               :confirmation_sent_at,
+  #               :unconfirmed_email,
+  #               :username,
+  #               :profile,
+  #               :icon
   #
   # or
   #
   # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :username, :profile, :icon]
+  # permitted = %i[email
+  #                encrypted_password
+  #                reset_password_token
+  #                reset_password_sent_at
+  #                remember_created_at
+  #                confirmation_token
+  #                confirmed_at
+  #                confirmation_sent_at
+  #                unconfirmed_email
+  #                username
+  #                profile
+  #                icon]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -18,6 +18,5 @@
 #
 FactoryBot.define do
   factory :admin_user do
-    
   end
 end
