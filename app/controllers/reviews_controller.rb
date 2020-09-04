75c0704b14cc2bddc3af310578f29e1bbd4f4418
tomoron <tomoron326@outlook.jp>
2020-09-04 17:52:32 +0900

    rubocop

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,6 @@ class ReviewsController < ApplicationController
   private
 
     def review_params
-      params.require(:review).permit(:title, :rate,:content, :product_id, :user_id)
+      params.require(:review).permit(:title, :rate, :content, :product_id, :user_id)
     end
 end
