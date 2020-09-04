b68d716ff77d026060e9cf8e653b12cb4599e3bf
tomoron <tomoron326@outlook.jp>
2020-09-04 22:09:23 +0900

    pref:like_button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,14 @@
 class LikesController < ApplicationController
   def create
-    like = Like.new(user_id: current_user.id, product_id: params[:product_id])
-    like.save
-    redirect_to product_path(params[:product_id])
+    @like = Like.new(user_id: current_user.id, product_id: params[:product_id])
+    @like.save
+
+
   end
 
   def destroy
     like = Like.find_by(id: params[:id])
+    @product = Product.find_by(id: like.product_id)
     like.destroy
-    redirect_to products_path
   end
 end
