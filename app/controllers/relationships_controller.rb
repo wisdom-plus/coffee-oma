af4ef6540ef6d6b60aebe3e8f3148c7b4cf2234b
tomoron <tomoron326@outlook.jp>
2020-09-05 20:32:43 +0900

    test:system_Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,11 +3,13 @@ class RelationshipsController < ApplicationController
     @follow = Relationship.new(user_id: current_user.id, follow_id: params[:follow_id]) unless current_user.id == params[:follow_id]
     @follow.save
     @user = User.find(params[:follow_id])
+    render 'create.js.erb'
   end
 
   def destroy
     @follow = Relationship.find(params[:id])
     @user = User.find(@follow.follow_id)
     @follow.destroy
+    render 'destroy.js.erb'
   end
 end
