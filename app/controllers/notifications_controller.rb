a0ef0d7ac520d56acdd51acb5ef1553f623e7298
tomoron <tomoron326@outlook.jp>
2020-09-20 22:46:19 +0900

    style:notification_index_tab_fiest

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,8 @@
 class NotificationsController < ApplicationController
   def index
+    @notifications = current_user.passive_notifications.includes([:visitor]).page(params[:page]).per(10)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
   end
 end
