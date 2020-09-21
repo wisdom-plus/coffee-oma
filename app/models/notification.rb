7691fd3df4d0e1ebf1cc0910c1589d4869b5189d
tomoron <tomoron326@outlook.jp>
2020-09-21 22:16:31 +0900

    style:notificatio_index

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -21,4 +21,5 @@ class Notification < ApplicationRecord
   belongs_to :visitor, class_name: "User", foreign_key: "visitor_id", optional: true
   belongs_to :visited, class_name: "User", foreign_key: "visited_id", optional: true
 
+  default_scope -> { order(created_at: :desc)}
 end
