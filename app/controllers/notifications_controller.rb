33a3d0779b742ae94186f2230366e4dcd8737b21
tomoron <tomoron326@outlook.jp>
2020-09-23 15:10:27 +0900

    fix:updates_attribute

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.includes([:visitor],[:review]).page(params[:page]).per(10)
     @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+      notification.update(checked: true)
     end
   end
 end
