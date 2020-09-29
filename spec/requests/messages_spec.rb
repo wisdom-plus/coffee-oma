86dc38737f9e0ea5d0a947f6429997210cb60356
tomoron <tomoron326@outlook.jp>
2020-09-29 16:06:31 +0900

    test:request_message

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,18 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
+  let(:user) { create(:user)}
+  let(:user1) { create(:user, email: 'test1@example.com', username: 'test2') }
+  let(:room) { create(:room, participant1: user, participant2: user1)}
+  let(:message) { create(:message, user: user, room: room)}
+  let(:message_params) { attributes_for(:message, user_id: user.id, room_id: room.id)}
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/messages/create"
-      expect(response).to have_http_status(:success)
+  describe 'POST create' do
+    before do
+      user.confirm
+      sign_in user
+      room
     end
-  end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/messages/new"
-      expect(response).to have_http_status(:success)
+    it 'request message' do
+      post messages_path, params: { message: message_params}
+      expect(response).to have_http_status(:found)
+    end
+
+    it "created message" do
+      expect do
+        post messages_path, params: { message: message_params }
+      end.to change(Message, :count).by 1
     end
   end
 
