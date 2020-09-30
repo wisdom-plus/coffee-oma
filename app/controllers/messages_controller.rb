185eef39eec5b4bcc2f0233fc7d1001477f50a00
tomoron <tomoron326@outlook.jp>
2020-09-30 22:42:44 +0900

    add:notification_message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.new(message_params)
     message.save
+    message.create_notification_message(current_user)
     redirect_to room_path(message.room_id)
   end
 
