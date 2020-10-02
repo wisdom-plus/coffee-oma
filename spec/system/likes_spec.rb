3c1a41a722fd96fc37256543067d9b6a64ac5b6d
tomoron <tomoron326@outlook.jp>
2020-10-02 18:21:47 +0900

    pref:test_system_like

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -6,14 +6,10 @@ RSpec.describe 'Likes', type: :system do
   let(:like) { create(:like, user: user, product: product) }
 
   describe 'create' do
-    context 'signed' do
+    context 'when login' do
       before do
         login(user, user.email, user.password)
         visit product_path(product.id)
-      end
-
-      it 'render button' do
-        expect(page).to have_link 'お気に入り登録'
       end
 
       it 'click like button' do
@@ -22,7 +18,7 @@ RSpec.describe 'Likes', type: :system do
       end
     end
 
-    context 'not signed' do
+    context 'when not login' do
       before do
         visit product_path(product.id)
       end
@@ -34,24 +30,20 @@ RSpec.describe 'Likes', type: :system do
   end
 
   describe 'destroy' do
-    context 'signed' do
+    context 'when login' do
       before do
         login(user, user.email, user.password)
         like
         visit product_path(product.id)
       end
 
-      it 'reder button' do
-        expect(page).to have_link 'お気に入り登録中'
-      end
-
-      it 'click button' do
+      it 'click like destroy button' do
         click_on 'お気に入り登録中'
         expect(page).to have_link 'お気に入り登録'
       end
     end
 
-    context 'not signed' do
+    context 'when not login' do
       before do
         like
         visit product_path(product.id)
