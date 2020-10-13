0c7d7138b6a395fede398643526dfc60be0735f5
tomoron <tomoron326@outlook.jp>
2020-10-13 22:45:45 +0900

    fix:gon

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.passive_notifications.includes([:visitor], [:review], [:message]).page(params[:page]).per(10)
+    @notifications = current_user.passive_notifications.includes([:visitor], [:review], [:message]).page(params[:page]).per(25)
     @notifications.where('checked = ?', false).each do |notification|
       notification.update(checked: true)
     end
