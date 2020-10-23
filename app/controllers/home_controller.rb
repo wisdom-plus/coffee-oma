79650b6c9c10f7d68a98a1dd327e75fdc8c2b519
tomoron <tomoron326@outlook.jp>
2020-10-23 23:23:09 +0900

    fix:rspec

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def top
     @rankings = Product.all.order('likes_count desc').limit(3)
-    @reviews = Review.all.includes([:product],[:user]).last(3)
+    @reviews = Review.all.includes([:product], [:user]).last(3)
   end
 
   def private_policy; end
