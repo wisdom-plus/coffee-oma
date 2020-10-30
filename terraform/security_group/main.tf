f1ee4fffd74c46a08809752c2df0003180d63346
tomoron <tomoron326@outlook.jp>
2020-10-30 18:33:22 +0900

    add:terraform

---
+++ terraform/security_group/main.tf
@@ -0,0 +1,33 @@
+variable "name" {}
+variable "vpc_id" {}
+variable "port" {}
+variable "cidr_blocks" {
+  type = list(string)
+}
+
+resource "aws_security_group" "example" { #セキュリティーグルーブの定義
+  name   = var.name
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "ingress_example" { #インバウンドの定義
+  type              = "ingress"
+  from_port         = var.port
+  to_port           = var.port
+  protocol          = "tcp"
+  cidr_blocks       = var.cidr_blocks
+  security_group_id = aws_security_group.example.id
+}
+
+resource "aws_security_group_id" "egress_example" { #アウトバウンドの定義
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.default.id
+}
