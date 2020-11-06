56a881e17247def50bd7a89a4a19a1499a0d795e
tomoron <tomoron326@outlook.jp>
2020-11-06 22:12:53 +0900

    add:unicorn

---
+++ src/confing/uicorn.rb
@@ -0,0 +1,12 @@
+worker_processes 2
+pid "/var/run/unicorn.pid"
+
+# developmentとproductionで場合分け
+if ENV['RAILS_ENV'] == 'production'
+  listen 3000
+else
+  listen "/share/unicorn.sock"
+end
+
+# タイムアウトまでの時間を伸ばす
+timeout 600
