460aaf655b602bd091411ed54a3b9013189912d7
tomoron <tomoron326@outlook.jp>
2020-11-18 16:25:53 +0900

    fix:env

--- rails/entrypoint.sh
+++ rails/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "$ENV_FILE" | base64 --decode >.env
+
+bundle exec rails assets:precompile RAILS_ENV=production
+
 bin/rails db:migrate RAILS_ENV=production
 
 exec "$@"
