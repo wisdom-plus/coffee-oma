e919ec7dcdd47ea512bfb4be309291538a67b6db
tomoron <tomoron326@outlook.jp>
2020-11-24 16:56:30 +0900

    add:aws_sdk_rails

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -109,6 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'coffee-oma.com' }
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.default_url_options = { host: 'coffee-oma.com', port: 3000 }
+  ActionMailer::Base.delivery_method = :ses
 end
