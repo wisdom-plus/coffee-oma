a8768e3113d5206df1e1e7b6592f11194ef8ff0e
tomoron <tomoron326@outlook.jp>
2020-11-24 10:05:19 +0900

    fix:rubocop

--- rails/config/initializers/aws.rb
+++ rails/config/initializers/aws.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.add_delivery_method :ses,
-  AWS::SES::Base,
-  access_key_id: ENV['AWS_SMTP_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SMTP_SECRET_ACCESS_KEY'],
-  server: 'email.ap-northeast-1.amazonaws.com'
+                                       AWS::SES::Base,
+                                       access_key_id: ENV['AWS_SMTP_ACCESS_KEY_ID'],
+                                       secret_access_key: ENV['AWS_SMTP_SECRET_ACCESS_KEY'],
+                                       server: 'email.ap-northeast-1.amazonaws.com'
