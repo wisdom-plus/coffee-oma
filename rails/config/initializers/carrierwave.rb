fbc7fce00558335bdf23e57364b499a3893788e0
tomoron <tomoron326@outlook.jp>
2020-11-24 16:56:30 +0900

    add:aws_sdk_rails

--- rails/config/initializers/carrierwave.rb
+++ rails/config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory =
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
-      path_style: true
-    }
+  config.fog_directory = 'coffee-oma-image-bucket'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1',
+    path_style: true
+  }
 end
