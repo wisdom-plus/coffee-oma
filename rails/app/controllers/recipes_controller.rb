a4604f17ccf3b7f090b52e62bcdf738049047e1e
tomoron <tomoron326@outlook.jp>
2020-12-09 22:40:08 +0900

    add:edit&index

--- rails/app/controllers/recipes_controller.rb
+++ rails/app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
-
-  def def index
-    @recipes = Recipe.all
+  before_action :authenticate_user!, only: [:new, :create,:edit, :update, :destory]
+  def index
+    @recipes = Recipe.where(['status = ?', true])
   end
 
   def new
@@ -9,31 +9,43 @@ class RecipesController < ApplicationController
   end
 
   def show
-
+    @recipe =  Recipe.find(params[:id])
   end
 
   def edit
-
+    @recipe = Recipe.find(params[:id])
   end
 
 
   def create
-    binding.pry
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to "/users/#{current_user.id}/show"
+    else
+      render :new
+    end
   end
 
   def destroy
-
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to "/user/#{current_user.id}/show"
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe.id)
+    else
+      render :edit
+    end
 
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:powdergram, :grinding, :temperature, :time, :amount, :time1, :time2)
+    params.require(:recipe).permit(:powdergram, :grinding, :temperature, :time, :amount, :time1, :time2, :status)
   end
 end
 
