74ce1092ccbf7e7b4fc215608eb8978c9c2e1d89
tomoron <tomoron326@outlook.jp>
2021-01-21 22:20:20 +0900

    add:admin_bean

---
+++ rails/app/admin/recipes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Recipe do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :powdergram, :grinding, :temperature, :time, :amount, :bean_review_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:powdergram, :grinding, :temperature, :time, :amount, :bean_review_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
