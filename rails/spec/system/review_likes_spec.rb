6fa9a0317af9316d40533b452f209eabb6688f84
tomoron <tomoron326@outlook.jp>
2021-02-07 17:50:12 +0900

    test:system_review_like

--- rails/spec/system/review_likes_spec.rb
+++ rails/spec/system/review_likes_spec.rb
@@ -4,41 +4,85 @@ RSpec.describe 'ReviewLikes', type: :system, js: true do
   let(:user) { create(:user) }
   let(:product) { create(:product) }
   let(:review) { create(:review, user: user, product: product) }
-  let(:review_like) { create(:review_like, user: user, review: review) }
+  let(:bean) { create(:bean, user: user) }
+  let(:bean_review) { create(:bean_review, user: user, bean: bean) }
+  let(:product_review_like) { create(:like, user: user, liked_id: review.id, type: 'ProductReviewLike') }
+  let(:bean_review_like) { create(:like, user: user, liked_id: bean_review.id, type: 'BeanReviewLike') }
 
   context 'when login' do
     context 'create' do
       before do
         login(user, user.email, user.password)
         review
-        visit product_path(product.id)
+        bean_review
       end
 
-      it 'display button' do
-        expect(page).to have_link nil, href: review_likes_path(review_id: review.id)
+      context 'product_review_like' do
+        before do
+          visit product_path(product.id)
+        end
+
+        it 'display button' do
+          expect(page).to have_link nil, href: review_likes_path(review_id: review.id, type: review.class)
+        end
+
+        it 'click like button' do
+          click_link nil, href: review_likes_path(review_id: review.id, type: review.class)
+          expect(page).to have_css '.destroy_button'
+        end
       end
 
-      it 'click like button' do
-        click_link nil, href: review_likes_path(review_id: review.id)
-        expect(page).to have_css '.destroy_button'
+      context 'bean_review_like' do
+        before do
+          visit bean_path(bean.id)
+        end
+
+        it 'display button' do
+          expect(page).to have_link nil, href: review_likes_path(review_id: bean_review.id, type: bean_review.class)
+        end
+
+        it 'click like button' do
+          click_link nil, href: review_likes_path(review_id: bean_review.id, type: bean_review.class)
+          expect(page).to have_css '.destroy_button'
+        end
       end
     end
 
     context 'destroy' do
       before do
         login(user, user.email, user.password)
-        review
-        review_like
-        visit product_path(product.id)
+        product_review_like
+        bean_review_like
       end
 
-      it 'reder button' do
-        expect(page).to have_link nil, href: review_like_path(review_like.id)
+      context 'product_review_like' do
+        before do
+          visit product_path(product.id)
+        end
+
+        it 'reder button' do
+          expect(page).to have_link nil, href: review_like_path(product_review_like.id, type: product_review_like.type)
+        end
+
+        it 'click button' do
+          click_link nil, href: review_like_path(product_review_like.id, type: product_review_like.type)
+          expect(page).to have_css '.create_button'
+        end
       end
 
-      it 'click button' do
-        click_link nil, href: review_like_path(review_like.id)
-        expect(page).to have_css '.create_button'
+      context 'bean_review_like' do
+        before do
+          visit bean_path(bean.id)
+        end
+
+        it 'reder button' do
+          expect(page).to have_link nil, href: review_like_path(bean_review_like.id, type: bean_review_like.type)
+        end
+
+        it 'click button' do
+          click_link nil, href: review_like_path(bean_review_like.id, type: bean_review_like.type)
+          expect(page).to have_css '.create_button'
+        end
       end
     end
   end
