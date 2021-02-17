b43e5d1956be5e12652891d969dee9e1440e7dfe
tomoron <tomoron326@outlook.jp>
2021-02-17 22:35:15 +0900

    style:history_index

--- rails/app/controllers/histories_controller.rb
+++ rails/app/controllers/histories_controller.rb
@@ -1,6 +1,6 @@
 class HistoriesController < ApplicationController
   def index
-    @histories = current_user.histories.order(updated_at: :DESC)
+    @histories = current_user.histories.includes([:bean, :product]).order(updated_at: :DESC)
 
   end
 end
