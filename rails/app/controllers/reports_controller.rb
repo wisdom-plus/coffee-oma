2d7ca212c60530b42d9594f7421e88ab368bd639
tomoron <tomoron326@outlook.jp>
2021-02-22 23:12:52 +0900

    pref:devise_mailer_to_active_job

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -3,7 +3,6 @@ class ReportsController < ApplicationController
 
   def create
     @report = current_user.reports.find_or_create_by(review_id: params[:review_id])
-    @report.save
     @review = Review.find(params[:review_id])
     if @review.reports.size > 10
       DeleteReviewJob.perform_later(params[:review_id])
