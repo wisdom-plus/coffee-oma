71c9256056d6d3893348bb49b889ad9a0d9f2ced
tomoron <tomoron326@outlook.jp>
2021-03-10 23:29:52 +0900

    add:pubic_seed

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+20.times do |n|
+  User.create!(email: Faker::Internet.email,
+              password: Faker::Internet.password,
+              username: Faker::Internet.username,
+              confirmed_at: Time.zone.now
+  )
+end
