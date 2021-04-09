a9e73706dcb0be9715aaef6174e40fabf4959c58
tomoron <tomoron326@outlook.jp>
2021-04-09 18:05:04 +0900

    fix:bean_like

--- rails/app/controllers/tags_controller.rb
+++ rails/app/controllers/tags_controller.rb
@@ -1,6 +1,24 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!
 
   def update
-
+    case params[:type]
+    when 'Product'
+      product = Product.find(params[:id])
+      product.tag_list.add(params[:tag_list],parse: true)
+      if product.save
+        redirect_to product_path(params[:id])
+      else
+        render product_path(params[:id])
+      end
+    when 'Bean'
+      bean = Bean.find(params[:id])
+      bean.tag_list.add(params[:tag_list],parse: true)
+      if bean.save
+        refirect_to bean_path(params[:id])
+      else
+        render bean_path(params[:id])
+      end
+    end
   end
 end
