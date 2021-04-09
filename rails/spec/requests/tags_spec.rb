a28fef945ef473aeb2daacd6ddffe5b739312399
tomoron <tomoron326@outlook.jp>
2021-04-10 00:03:21 +0900

    fix:rucocop

--- rails/spec/requests/tags_spec.rb
+++ rails/spec/requests/tags_spec.rb
@@ -3,20 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Tags', type: :request do
   let(:user) { create(:user) }
   let(:product) { create(:product) }
-  let(:bean) { create(:bean, user: user)}
-  let(:tag_list) {'coffee bean'}
+  let(:bean) { create(:bean, user: user) }
+  let(:tag_list) { 'coffee bean' }
 
   describe 'PATCH /tag' do
     it 'request success(product)' do
-      patch tag_path(product.id), params: {type: 'Product', tag_list: tag_list}
+      patch tag_path(product.id), params: { type: 'Product', tag_list: tag_list }
       expect(response).to have_http_status(:found)
     end
 
     it 'request success(bean)' do
-      patch tag_path(bean.id), params: {type: 'Bean', tag_list: tag_list}
+      patch tag_path(bean.id), params: { type: 'Bean', tag_list: tag_list }
       expect(response).to have_http_status(:found)
     end
   end
-
-
 end
