d1e9aa8cd4cd585563fd222c3d5864ed37bbe4b6
tomoron <tomoron326@outlook.jp>
2021-04-16 23:44:08 +0900

    add:tag_api

--- rails/app/models/tag.rb
+++ rails/app/models/tag.rb
@@ -13,4 +13,8 @@
 #  index_tags_on_name  (name) UNIQUE
 #
 class Tag < ApplicationRecord
+  scope :tag_search, ->(keyword) do
+    return if keyword.nil?
+    where('name LIKE ?', "#{keyword}%")
+  end
 end
