868bb1127b9161f084c6dc9d4c55f61c5bf85b0d
tomoron <tomoron326@outlook.jp>
2021-04-26 18:44:28 +0900

    add:bean_review_report

--- rails/app/models/report.rb
+++ rails/app/models/report.rb
@@ -2,23 +2,23 @@
 #
 # Table name: reports
 #
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  review_id  :bigint           not null
-#  user_id    :bigint           not null
+#  id          :bigint           not null, primary key
+#  review_type :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  review_id   :bigint           not null
+#  user_id     :bigint           not null
 #
 # Indexes
 #
-#  index_reports_on_review_id  (review_id)
-#  index_reports_on_user_id    (user_id)
+#  index_reports_on_review_type_and_review_id  (review_type,review_id)
+#  index_reports_on_user_id                    (user_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (review_id => reviews.id)
 #  fk_rails_...  (user_id => users.id)
 #
 class Report < ApplicationRecord
   belongs_to :user
-  belongs_to :review
+  belongs_to :review, polymorphic: true
 end
