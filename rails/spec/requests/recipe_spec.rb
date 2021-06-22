f5bb2ebd518f31e3bdb89572497ebf00d70deb75
tomoron <tomoron326@outlook.jp>
2021-06-22 16:23:20 +0900

    test:request_recipe

---
+++ rails/spec/requests/recipe_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'BeanReviews', type: :request do
+  let(:user) { create(:user) }
+  let(:bean) { create(:bean, user: user) }
+  let(:bean_review) { create(:bean_review, user: user, bean: bean) }
+  let(:recipe) { create(:recipe, bean_review: bean_review)}
+  let(:bean_review_params) { attributes_for(:bean_review,amount: '220',grinding: '中細挽き',powdergram: '22',temperature: '95',time1: '2',time2: '30') }
+
+
+  describe 'POST create' do
+    before do
+      user.confirm
+      sign_in user
+      bean
+    end
+
+    it 'request success' do
+      post bean_bean_reviews_path(bean.id), params: { bean_review: bean_review_params }
+      expect(response).to have_http_status(:found)
+    end
+
+    it 'review created ' do
+      expect do
+        post bean_bean_reviews_path(bean.id), params: { bean_review: bean_review_params }
+      end.to change(Recipe, :count).by 1
+    end
+  end
+end
