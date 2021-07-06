a6a36790d976ede35c9219557c40435a6834eba1
tomoron <tomoron326@outlook.jp>
2021-07-06 14:35:53 +0900

    pref:bean_controller

--- rails/app/controllers/messages_controller.rb
+++ rails/app/controllers/messages_controller.rb
@@ -1,12 +1,10 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-
+  after_action :message_notification
   def create
     @message = current_user.messages.new(message_params)
     return unless @message.save
 
-    @messages = Message.where('room_id = ?', params[:room_id])
-    @message.create_notification_message(current_user)
     ActionCable.server.broadcast("room_channel_#{@message.room_id}",
                                  { message: @message.message.gsub(/\r\n|\r|\n/, '<br>'),
                                    user_id: current_user.id,
@@ -18,5 +16,9 @@ class MessagesController < ApplicationController
 
     def message_params
       params.require(:message).permit(:message).merge(room_id: params[:room_id])
+    end
+
+    def message_notification
+      @message.create_notification_message(current_user) if @message.save
     end
 end
--- rails/app/controllers/notifications_controller.rb
+++ rails/app/controllers/notifications_controller.rb
@@ -1,13 +1,19 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
+  after_action :checked_notifications
 
   def index
     @notifications = current_user.passive_notifications.includes([:visitor], [:review], [:message]).page(params[:page]).per(25)
-    @notifications.where('checked = ?', false).each do |notification|
-      notification.update(checked: true)
-    end
     @follow_notifications = @notifications.where('action = ?', 'follow')
     @like_notifications = @notifications.where('action = ?', 'like')
     @message_notifications = @notifications.where('action = ?', 'message')
+  end
+
+  private
+
+  def checked_notifications
+    @notifications.where('checked = ?', false).each do |notification|
+      notification.update(checked: true)
+    end
   end
 end
