610294661ac4f0126255fca52ceb26c1381a27f9
tomoron <tomoron326@outlook.jp>
2021-07-10 20:46:19 +0900

    upgrade

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,14 @@
 terraform {
-  required_version = ">=0.13"
+  required_version = "~> 1.0.2"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.38.0"
+    }
+  }
 }
 
 provider aws {
-  version = "~> 3.13.0"
   region  = var.aws_region
 }
