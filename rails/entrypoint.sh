a2fd41789c03a6e6bf8b23f3f78e405c65b61786
tomoron <tomoron326@outlook.jp>
2021-07-13 13:07:52 +0900

    fix:config

--- rails/entrypoint.sh
+++ rails/entrypoint.sh
@@ -3,12 +3,8 @@ set -e
 
 echo "$ENV_FILE" | base64 -d >.env
 
-bin/rails db:migrate:reset RAILS_ENV=production
-
-bin/rails db:seed RAILS_ENV=production
+bin/rails db:migrate RAILS_ENV=production
 
 RAILS_ENV=production bin/delayed_job -n 2 start
-
-whenever -i
 
 exec "$@"
