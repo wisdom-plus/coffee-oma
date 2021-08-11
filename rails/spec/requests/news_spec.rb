6889b3c20725970243ac48558378347116baa728
tomoron <tomoron326@outlook.jp>
2021-08-11 11:08:35 +0900

    add:action_text

--- rails/spec/requests/news_spec.rb
+++ rails/spec/requests/news_spec.rb
@@ -1,22 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "News", type: :request do
+  let(:news) { create(:news)}
+
   describe "GET /news" do
+    it 'request success' do
+      get news_index_path
+      expect(response).to have_http_status(:ok)
+    end
 
   end
-  describe "GET /news" do
+
+  describe "POST /news" do
 
   end
-  describe "GET /news" do
+
+  describe "GET /news/new" do
+    it 'request success' do
+      get new_news_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+  describe "GET /news/:id/edit" do
 
   end
-  describe "GET /news" do
+  describe "GET /news/:id" do
+    it "request success" do
+      get news_path(news.id)
+      expect(response).to have_http_status(:ok)
+    end
 
   end
-  describe "GET /news" do
+  describe "PUT /news/:id" do
 
   end
-  describe "GET /news" do
+  describe "DELETE /news/:id" do
 
   end
 end
