9f9fc3917797d728f7a2c1152530177e67ea45db
tomoron <tomoron326@outlook.jp>
2021-08-11 09:24:51 +0900

    add:routes

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_031607) do
+ActiveRecord::Schema.define(version: 2021_08_10_074943) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
@@ -126,6 +126,16 @@ ActiveRecord::Schema.define(version: 2021_07_02_031607) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "news", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content", null: false
+    t.boolean "active", default: false
+    t.datetime "publicshed_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_news_on_user_id"
   end
 
   create_table "notifications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -264,6 +274,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_031607) do
 
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "news", "admin_users", column: "user_id"
   add_foreign_key "notifications", "messages"
   add_foreign_key "notifications", "reviews"
   add_foreign_key "notifications", "users", column: "visited_id"
