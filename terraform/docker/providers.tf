90b819be75ae8b2cef13d1266712fe2f880e14af
tomoron <tomoron326@outlook.jp>
2021-09-07 23:00:26 +0900

    add:terrafomr_docker

---
+++ terraform/docker/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.0.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
