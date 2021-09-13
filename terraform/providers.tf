526f5b845b00454970cf2fa7d2c53ec25693920e
tomoron <tomoron326@outlook.jp>
2021-09-13 23:33:55 +0900

    add:ecs_task

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,8 +7,18 @@ terraform {
       version = "3.38.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-portfolio-tfstate"
+    key = "terraform.tfstate"
+    encrypt = true
+    region = "ap-northeast-1"
+    access_key = "AKIA4QXCQYNJIHXMI65G"
+    secret_key = "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"
+  }
 }
 
 provider "aws" {
   region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
