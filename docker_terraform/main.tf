651801e3099c14b44b33018e20aff0789725ba1c
tomoron <tomoron326@outlook.jp>
2021-09-13 23:33:34 +0900

    add:data

--- docker_terraform/main.tf
+++ docker_terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_elasticache_parameter_group" "portfolio-parameter-group" {
 
 resource "aws_elasticache_subnet_group" "portfolio-subnet-group" {
   name       = "portfolio-subnet-group"
-  subnet_ids = [aws_subnet.portfolio-private-subnet-1.id, aws_subnet.portfolio-private-subnet-2.id]
+  subnet_ids = [data.aws_subnet.portfolio-private-subnet-1.id, data.aws_subnet.portfolio-private-subnet-2.id]
 }
 
 resource "aws_elasticache_replication_group" "portfolio-replication-group" {
@@ -32,23 +32,23 @@ resource "aws_elasticache_replication_group" "portfolio-replication-group" {
 module "redis_sg" {
   source      = "./security_group"
   name        = "redis-sg"
-  vpc_id      = aws_vpc.portfolio-vpc.id
+  vpc_id      = data.aws_vpc.portfolio-vpc.id
   port        = 6379
-  cidr_blocks = [aws_vpc.portfolio-vpc.cidr_block]
+  cidr_blocks = [data.aws_vpc.portfolio-vpc.cidr_block]
 }
 
 
 
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = aws_vpc.portfolio-vpc.id
+  vpc_id            = data.aws_vpc.portfolio-vpc.id
   service_name      = data.aws_vpc_endpoint_service.s3.service_name
   vpc_endpoint_type = "Gateway"
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
-  route_table_id  = aws_route_table.private-route-table-1.id
+  route_table_id  = data.aws_route_table.private-route-table-1.id
 }
 
 data "aws_vpc_endpoint_service" "s3" {
@@ -57,12 +57,12 @@ data "aws_vpc_endpoint_service" "s3" {
 }
 
 resource "aws_vpc_endpoint" "dkr" {
-  vpc_id            = aws_vpc.portfolio-vpc.id
+  vpc_id            = data.aws_vpc.portfolio-vpc.id
   service_name      = data.aws_vpc_endpoint_service.dkr.service_name
   vpc_endpoint_type = "Interface"
 
   security_group_ids  = [module.http_sg.security_group_id, module.https_sg.security_group_id]
-  subnet_ids          = [aws_subnet.portfolio-private-subnet-1.id]
+  subnet_ids          = [data.aws_subnet.portfolio-private-subnet-1.id]
   private_dns_enabled = true
 }
 
@@ -71,12 +71,12 @@ data "aws_vpc_endpoint_service" "dkr" {
 }
 
 resource "aws_vpc_endpoint" "ecr-api" {
-  vpc_id            = aws_vpc.portfolio-vpc.id
+  vpc_id            = data.aws_vpc.portfolio-vpc.id
   service_name      = data.aws_vpc_endpoint_service.ecr-api.service_name
   vpc_endpoint_type = "Interface"
 
   security_group_ids  = [module.http_sg.security_group_id, module.https_sg.security_group_id]
-  subnet_ids          = [aws_subnet.portfolio-private-subnet-1.id]
+  subnet_ids          = [data.aws_subnet.portfolio-private-subnet-1.id]
   private_dns_enabled = true
 }
 
@@ -84,12 +84,12 @@ data "aws_vpc_endpoint_service" "ecr-api" {
   service = "ecr.api"
 }
 resource "aws_vpc_endpoint" "logs" {
-  vpc_id            = aws_vpc.portfolio-vpc.id
+  vpc_id            = data.aws_vpc.portfolio-vpc.id
   service_name      = data.aws_vpc_endpoint_service.logs.service_name
   vpc_endpoint_type = "Interface"
 
   security_group_ids  = [module.http_sg.security_group_id, module.https_sg.security_group_id]
-  subnet_ids          = [aws_subnet.portfolio-private-subnet-1.id]
+  subnet_ids          = [data.aws_subnet.portfolio-private-subnet-1.id]
   private_dns_enabled = true
 
 }
@@ -99,12 +99,12 @@ data "aws_vpc_endpoint_service" "logs" {
 }
 
 resource "aws_vpc_endpoint" "email-smtp" {
-  vpc_id            = aws_vpc.portfolio-vpc.id
+  vpc_id            = data.aws_vpc.portfolio-vpc.id
   service_name      = data.aws_vpc_endpoint_service.email-smtp.service_name
   vpc_endpoint_type = "Interface"
 
   security_group_ids  = [module.http_sg.security_group_id, module.https_sg.security_group_id, module.stmp_sg.security_group_id]
-  subnet_ids          = [aws_subnet.portfolio-private-subnet-1.id]
+  subnet_ids          = [data.aws_subnet.portfolio-private-subnet-1.id]
   private_dns_enabled = true
 }
 
@@ -115,7 +115,7 @@ data "aws_vpc_endpoint_service" "email-smtp" {
 module "stmp_sg" {
   source      = "./security_group"
   name        = "stmp-sg"
-  vpc_id      = aws_vpc.portfolio-vpc.id
+  vpc_id      = data.aws_vpc.portfolio-vpc.id
   port        = 587
   cidr_blocks = ["0.0.0.0/0"]
 }
@@ -123,7 +123,23 @@ module "stmp_sg" {
 module "stmp_sg2" {
   source      = "./security_group"
   name        = "stmp-sg2"
-  vpc_id      = aws_vpc.portfolio-vpc.id
+  vpc_id      = data.aws_vpc.portfolio-vpc.id
   port        = 465
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_sg" {
+  source      = "./security_group"
+  name        = "http-sg"
+  vpc_id      = data.aws_vpc.portfolio-vpc.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source      = "./security_group"
+  name        = "https-sg"
+  vpc_id      = data.aws_vpc.portfolio-vpc.id
+  port        = 443
   cidr_blocks = ["0.0.0.0/0"]
 }
