d99c68a5552245e1c0634244ab06ad277d3d0835
tomoron <tomoron326@outlook.jp>
2021-09-14 13:44:03 +0900

    add:command

--- docker_terraform/entrypoint-create.sh
+++ docker_terraform/entrypoint-create.sh
@@ -5,6 +5,6 @@ echo "$TFVAR_FILE" | base64 -d >terraform.tfvars
 
 terraform init
 
-terraform apply --target=aws_elasticache_replication_group.portfolio-replication-group -auto-approve
+terraform apply --target=aws_elasticache_replication_group.portfolio-replication-group　--target=aws_vpc_endpoint.s3　--target=aws_vpc_endpoint.dkr　--target=aws_vpc_endpoint.ecr-api　--target=aws_vpc_endpoint.logs　--target=aws_vpc_endpoint.email-smtp　-auto-approve -parallelism=20
 
 exec "$@"
