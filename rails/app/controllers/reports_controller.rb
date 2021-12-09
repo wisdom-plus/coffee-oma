8738fcad744afcfeea240117f6ad74612c3ad608
tomoron <tomoron326@outlook.jp>
2021-12-09 23:30:40 +0900

    fix:report_request_spec

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -4,10 +4,14 @@ class ReportsController < ApplicationController
   def create
     @review = if params[:type] == 'Review'
                 Review.find_reviews(params[:review_id])
-              else
+              elsif params[:type] == 'BeanReview'
                 BeanReview.find_reviews(params[:review_id])
               end
     Report.create_report(current_user, @review)
-    redirect_to product_path(@review.product_id), notice: 'レビューを通報しました。'
+    if params[:type] == 'Review'
+      redirect_to product_path(@review.product_id), notice: 'レビューを通報しました。'
+    elsif params[:type] == 'BeanReview'
+      redirect_to bean_path(@review.bean_id), notice: 'レビューを通報しました。'
+    end
   end
 end
