7bf269498e3c9a81d09bb7abb65c4293378642b4
tomoron <tomoron326@outlook.jp>
2021-12-09 22:06:41 +0900

    fix:rubocop

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -8,6 +8,6 @@ class ReportsController < ApplicationController
                 BeanReview.find_reviews(params[:review_id])
               end
     Report.create_report(current_user, @review)
-    redirect_to product_path(@review.product_id),notice: 'レビューを通報しました。'
+    redirect_to product_path(@review.product_id), notice: 'レビューを通報しました。'
   end
 end
