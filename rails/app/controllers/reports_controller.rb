e41fddede5edead13e09bd25ea5bde22a5a053a5
tomoron <tomoron326@outlook.jp>
2021-12-10 11:59:36 +0900

    fix:rubocop

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -2,15 +2,17 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @review = if params[:type] == 'Review'
+    @review = case params[:type]
+              when 'Review'
                 Review.find_reviews(params[:review_id])
-              elsif params[:type] == 'BeanReview'
+              when 'BeanReview'
                 BeanReview.find_reviews(params[:review_id])
               end
     Report.create_report(current_user, @review)
-    if params[:type] == 'Review'
+    case params[:type]
+    when 'Review'
       redirect_to product_path(@review.product_id), notice: 'レビューを通報しました。'
-    elsif params[:type] == 'BeanReview'
+    when 'BeanReview'
       redirect_to bean_path(@review.bean_id), notice: 'レビューを通報しました。'
     end
   end
