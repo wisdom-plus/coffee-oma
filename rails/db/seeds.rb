ed9550b1190fa80f762a7caff4ff74086dfc5160
tomoron <tomoron326@outlook.jp>
2021-12-16 10:26:10 +0900

    pref:bean_review_count

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1045,6 +1045,14 @@ end
       user: User.find(n+1),
       follow: User.find(1)
     )
+    ProductLike.create!(
+      user: User.find(n +1),
+      liked_id: 1
+    )
+    BeanLike.create!(
+      user: User.find(n +1),
+      liked_id: 1
+    )
   end
   ProductLike.create!(
     user: User.find(n +1),
@@ -1054,15 +1062,15 @@ end
     user: User.find(n +2),
     liked_id: n +1
   )
-  if n < 4
-    BeanLike.create!(
-      user: User.find(n +1),
-      liked_id: n + 1
-    )
-    BeanLike.create!(
-      user: User.find(n+2),
-      liked_id: n+1
-    )
+  BeanLike.create!(
+    user: User.find(n +1),
+    liked_id: n + 1
+  )
+  BeanLike.create!(
+    user: User.find(n+2),
+    liked_id: n+1
+  )
+  if n < 6
     ProductReviewLike.create!(
       user: User.find(n +3),
       liked_id: 1
