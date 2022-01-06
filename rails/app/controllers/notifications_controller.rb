026e305ce2b33951cc0569e700b2b357ee3e0882
tomoron <tomoron326@outlook.jp>
2022-01-06 23:07:37 +0900

    fix:like_notification_include

--- rails/app/controllers/notifications_controller.rb
+++ rails/app/controllers/notifications_controller.rb
@@ -3,10 +3,32 @@ class NotificationsController < ApplicationController
   after_action -> { Notification.checked_notifications(current_user.id) }
 
   def index
-    @notifications = current_user.passive_notifications.includes(:visitor,:message, :like).page(params[:page]).per(25).history_order
+    @notifications = current_user.passive_notifications.includes(:visitor,:message).limit(25).history_order
     @follow_notifications = @notifications.action_filter('follow')
     @bean_review_like_notifications = @notifications.action_filter('BeanReviewLike')
     @product_review_like_notifications = @notifications.action_filter('ProductReviewLike')
     @message_notifications = @notifications.action_filter('message')
+    notification_include(@notifications)
+    review_like_include(@bean_review_like_notifications)
+    bean_review_like_include(@product_review_like_notifications)
   end
+
+  private
+
+  def notification_include(likes)
+    preloader = ActiveRecord::Associations::Preloader.new
+    preloader.preload(likes.select { |i| i.action == 'BeanReviewLike' }, like: :bean_review)
+    preloader.preload(likes.select { |i| i.action == 'ProductReviewLike' },like: :review)
+  end
+
+  def review_like_include(likes)
+    preloader = ActiveRecord::Associations::Preloader.new
+    preloader.preload(likes.select { |i| i.action == 'BeanReviewLike' }, like: :bean_review)
+  end
+
+  def bean_review_like_include(likes)
+    preloader = ActiveRecord::Associations::Preloader.new
+    preloader.preload(likes.select { |i| i.action == 'ProductReviewLike' },like: :review)
+  end
+
 end
