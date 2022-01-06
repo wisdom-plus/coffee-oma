7dde11a2d0bbe001ad1660c1e6fbfe72e6ad44df
tomoron <tomoron326@outlook.jp>
2022-01-06 22:44:52 +0900

    style:notification_index

--- rails/app/controllers/notifications_controller.rb
+++ rails/app/controllers/notifications_controller.rb
@@ -3,9 +3,10 @@ class NotificationsController < ApplicationController
   after_action -> { Notification.checked_notifications(current_user.id) }
 
   def index
-    @notifications = current_user.passive_notifications.includes(:visitor, :review, :message).page(params[:page]).per(25).history_order
+    @notifications = current_user.passive_notifications.includes(:visitor,:message, :like).page(params[:page]).per(25).history_order
     @follow_notifications = @notifications.action_filter('follow')
-    @like_notifications = @notifications.action_filter('like')
+    @bean_review_like_notifications = @notifications.action_filter('BeanReviewLike')
+    @product_review_like_notifications = @notifications.action_filter('ProductReviewLike')
     @message_notifications = @notifications.action_filter('message')
   end
 end
