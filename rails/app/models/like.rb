433c1f173f20d8b66f11be6850738a4afc8f0859
tomoron <tomoron326@outlook.jp>
2022-01-08 20:31:21 +0900

    registration_show_like_includes

--- rails/app/models/like.rb
+++ rails/app/models/like.rb
@@ -18,10 +18,22 @@ class Like < ApplicationRecord
   belongs_to :user
   has_many :notifications, dependent: :destroy
 
-  scope :find_product_or_bean, ->(user_id) { where(["user_id = ? and type IN ('ProductLike','BeanLike')", user_id]).order(created_at: :DESC) }
+  scope :find_product_and_bean, ->(user_id) { where(["user_id = ? and type IN ('ProductLike','BeanLike')", user_id]).order(created_at: :DESC) }
 
   def create_notification(current_user, user_id)
     notification = current_user.create_like_notification(id, user_id, self.class.name)
     notification.save
+  end
+
+  def self.product_and_bean_likes_includes(likes)
+    preloader = ActiveRecord::Associations::Preloader.new
+    preloader.preload(likes.select { |i| i.type == 'BeanLike' }, :bean)
+    preloader.preload(likes.select { |i| i.type == 'ProductLike' }, :product)
+  end
+
+  def self.like_include(user_id)
+    likes = self.find_product_and_bean(user_id)
+    product_and_bean_likes_includes(likes)
+    likes
   end
 end
