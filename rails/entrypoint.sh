2c72f99c322aabdc4980cfb837f83857ee41176a
tomoron <tomoron326@outlook.jp>
2022-01-20 14:04:01 +0900

    db:reset

--- rails/entrypoint.sh
+++ rails/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "$ENV_FILE" | base64 -d >.env
 
-bin/rails db:migrate RAILS_ENV=production
+bin/rails db:reset RAILS_ENV=production
 
 RAILS_ENV=production bin/delayed_job -n 2 start
 
