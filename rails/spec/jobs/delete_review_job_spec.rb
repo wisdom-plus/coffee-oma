75f44f5c55cf843b347cba6a52fbd68ba776ca1b
tomoron <tomoron326@outlook.jp>
2022-02-15 20:02:12 +0900

    add:job_spec

---
+++ rails/spec/jobs/delete_review_job_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'delete_review' do
+  let(:user) { create(:user) }
+  let(:product) { create(:product, user: user) }
+  let(:review) { create(:review, user: user, product: product) }
+
+  describe 'perform_later' do
+    it 'enqueue job' do
+      ActiveJob::Base.queue_adapter = :test
+      DeleteReviewJob.perform_later(review)
+      expect(DeleteReviewJob).to have_been_enqueued
+    end
+
+    it 'delete review' do
+      ActiveJob::Base.queue_adapter = :test
+      review
+      expect do
+        DeleteReviewJob.perform_now(review)
+      end.to change(Review, :count).by -1
+    end
+  end
+end
