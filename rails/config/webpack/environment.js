d71981e4d5351648dd32fcf55af6ded272a8fbf3
tomoron <tomoron326@outlook.jp>
2022-02-19 20:05:41 +0900

    config_

--- rails/config/webpack/environment.js
+++ rails/config/webpack/environment.js
@@ -1,10 +1,13 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-const webpack = require("webpack")
+const webpack = require("webpack");
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery/src/jquery',
-  jQuery: 'jquery/src/jquery',
-  Popper: ['popper.js', 'default']
-}))
-module.exports = environment
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
+module.exports = environment;
