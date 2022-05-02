cadec82aad5b1253e529661594f8f95dd86437a7
tomoron <tomoron326@outlook.jp>
2022-05-02 20:59:22 +0900

    change:type_intger

--- rails/db/migrate/20200925065937_create_messages.rb
+++ rails/db/migrate/20200925065937_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false,type: :bigint, foreign_key: true
+      t.references :room, null: false,type: :bigint, foreign_key: true
       t.text :message, null: false
       t.timestamps
     end
