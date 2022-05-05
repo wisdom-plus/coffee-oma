00913163553a6baefe3fcf7a7c174a07bc3c8a9f
tomoron <tomoron326@outlook.jp>
2022-05-05 21:08:34 +0900

    add:admin rooms

---
+++ rails/app/admin/rooms.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Room do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :participant1_id, :participant2_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:participant1_id, :participant2_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
