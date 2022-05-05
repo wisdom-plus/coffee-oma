887afab81416d9399b2b79a74fb07d45c96db9d0
tomoron <tomoron326@outlook.jp>
2022-05-05 21:07:34 +0900

    add:admin notifications

---
+++ rails/app/admin/notifications.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Notification do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :visitor_id, :visited_id, :action, :checked, :message_id, :like_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:visitor_id, :visited_id, :action, :checked, :message_id, :like_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
