767b459c5eac37e7fd7812aa51527e3d88e83b3c
tomoron <tomoron326@outlook.jp>
2022-05-06 20:34:09 +0900

    add:permit_pararams reports

--- rails/app/admin/reports.rb
+++ rails/app/admin/reports.rb
@@ -9,10 +9,10 @@ ActiveAdmin.register Report do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:user_id, :review_type, :review_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params do
+    permitted = [:user_id, :review_type, :review_id]
+    permitted << :other if params[:action] == 'create' && admin_user_signed_in?
+    permitted
+  end
+
 end
