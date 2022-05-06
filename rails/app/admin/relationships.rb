2c4cd9db1baaf0433abf174ab5dece0e5b0d196a
tomoron <tomoron326@outlook.jp>
2022-05-06 20:33:54 +0900

    add:permit_pararams relationships

--- rails/app/admin/relationships.rb
+++ rails/app/admin/relationships.rb
@@ -9,10 +9,10 @@ ActiveAdmin.register Relationship do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:user_id, :follow_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params do
+    permitted = [:user_id, :follow_id]
+    permitted << :other if params[:action] == 'create' && admin_user_signed_in?
+    permitted
+  end
+
 end
