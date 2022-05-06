a7ddcbbf8ac35fdc80dd70ac4b321644891b04c7
tomoron <tomoron326@outlook.jp>
2022-05-06 20:34:17 +0900

    add:permit_pararams rooms

--- rails/app/admin/rooms.rb
+++ rails/app/admin/rooms.rb
@@ -9,10 +9,10 @@ ActiveAdmin.register Room do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:participant1_id, :participant2_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params do
+    permitted = [:participant1_id, :participant2_id]
+    permitted << :other if params[:action] == 'create' && admin_user_signed_in?
+    permitted
+  end
+
 end
