a5e4d763cf6cad763c8206036dd717f382d1e9c4
tomoron <tomoron326@outlook.jp>
2022-05-06 20:32:04 +0900

    add:permit_pararams bean_likes

--- rails/app/admin/bean_likes.rb
+++ rails/app/admin/bean_likes.rb
@@ -9,10 +9,10 @@ ActiveAdmin.register BeanLike do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:user_id, :type, :liked_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params do
+    permitted = [:user_id, :type, :liked_id]
+    permitted << :other if params[:action] == 'create' && admin_user_signed_in?
+    permitted
+  end
+
 end
