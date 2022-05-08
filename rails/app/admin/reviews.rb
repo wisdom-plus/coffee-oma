0c0c09e3f292b8aec595a0be9e0ebb55c126e45f
tomoron <tomoron326@outlook.jp>
2022-05-08 22:53:28 +0900

    add:admin review

--- rails/app/admin/reviews.rb
+++ rails/app/admin/reviews.rb
@@ -8,6 +8,41 @@ ActiveAdmin.register Review do
   #
   # or
   #
+
+  index do
+    column :id
+    column :title
+    column :rate
+    column :content
+    column :reviewlikes_count
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do |review|
+    attributes_table(*review.class.columns.collect { |column| column.name.to_sym })
+    panel 'お気に入り' do
+      table_for review.product_review_likes do
+        column :id
+        column 'ユーザー名' do |like|
+          like.user.username
+        end
+        column :created_at
+      end
+    end
+
+    panel '商品' do
+      table_for review.product do
+        column :id
+        column :itemname
+        column :itemprice
+        column :shopname
+        column :itemcaption
+      end
+    end
+  end
+
   permit_params do
     permitted = %i[user_id product_id title content rate]
     permitted << :other if params[:action] == 'create' && admin_user_signed_in?
