7f2dee2083ef033d071f8fbba876d910ff606d16
tomoron <tomoron326@outlook.jp>
2022-05-10 10:54:18 +0900

    add:admin bean_like hsow

--- rails/app/admin/bean_likes.rb
+++ rails/app/admin/bean_likes.rb
@@ -8,6 +8,19 @@ ActiveAdmin.register BeanLike do
   #
   # or
   #
+
+  show do |bean_like|
+    attributes_table(*bean_like.class.columns.collect { |column| column.name.to_sym })
+    panel 'ユーザー' do
+      table_for bean_like.user do
+        column :id
+        column :username
+        column :created_at
+      end
+    end
+    active_admin_comments
+  end
+
   permit_params do
     permitted = %i[user_id type liked_id]
     permitted << :other if params[:action] == 'create' && admin_user_signed_in?
