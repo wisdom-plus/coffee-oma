9dbe2c7f66d2334140f3627f8663ef3956bd5bd0
tomoron <tomoron326@outlook.jp>
2022-05-11 13:27:14 +0900

    fix:rubocop

--- rails/spec/requests/registrations_spec.rb
+++ rails/spec/requests/registrations_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Registrations', type: :request do
   let(:user) { create(:user) }
   let(:user1) { create(:user, email: 'test1@example.com', username: 'test2') }
+
   let_it_be(:user_params) { attributes_for(:user) }
   let_it_be(:user_update_params) { attributes_for(:user, username: 'test') }
 
