fe9ced574f58442500e69cd467a58c848ae27750
tomoron <tomoron326@outlook.jp>
2022-05-11 19:10:41 +0900

    test:model tag

--- rails/spec/models/tag_spec.rb
+++ rails/spec/models/tag_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'scope' do
+    it 'tag_search' do
+      tag = create(:tag, name: 'tag1')
+      expect(Tag.tag_search('tag')).to include(tag)
+    end
+
+    it 'tag_search(nil)' do
+      tag = create(:tag, name: 'tag1')
+      tag2 = create(:tag, name: 'tag2')
+      expect(Tag.tag_search(nil)).to include(tag, tag2)
+    end
+  end
 end
