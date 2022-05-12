8a292b17c0199665b68919c81aa57ec3eddf74af
tomoron <tomoron326@outlook.jp>
2022-05-12 14:22:50 +0900

    add:admin bean_review system spec

---
+++ rails/spec/system/admin_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+Rspec.describe 'Admin', type: :request do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:bean_review) { create(:bean_review, user: user) }
+
+  describe 'dashborad' do
+    before do
+      admin_login(admin)
+      visit admin_root_path
+    end
+
+    it 'display dashborad' do
+      expect(page).to have_content 'ダッシュボード'
+      expect(page).to have_current_path admin_root_path
+    end
+  end
+
+  describe 'bean_review' do
+    before do
+      admin_login(admin)
+      bean_review
+      visit admin_bean_reviews_path
+    end
+
+    it 'displayed index' do
+      expect(page).to have_content 'コーヒー豆レビュー'
+    end
+
+    it 'displayed show' do
+      visit admin_bean_review_path(bean_review.id)
+      expect(page).to have_content bean_review.title
+    end
+  end
+end
