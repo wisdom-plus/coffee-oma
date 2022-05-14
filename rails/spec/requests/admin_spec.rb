2c91de798bdb040f7fea16997ad9a94b30de0ad0
tomoron <tomoron326@outlook.jp>
2022-05-14 21:41:17 +0900

    fix:rubocop

--- rails/spec/requests/admin_spec.rb
+++ rails/spec/requests/admin_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Admin', type: :request do
   let(:admin) { create(:admin_user) }
+
   let_it_be(:user) { create(:user) }
   let_it_be(:bean) { create(:bean, user: user) }
   let_it_be(:bean_review) { create(:bean_review, user: user, bean: bean) }
@@ -16,7 +17,7 @@ RSpec.describe 'Admin', type: :request do
 
     it 'created resource' do
       expect do
-        post admin_bean_reviews_path,params: { bean_review: attributes_for(:bean_review,bean_id: bean.id,user_id: user.id) }
+        post admin_bean_reviews_path, params: { bean_review: attributes_for(:bean_review, bean_id: bean.id, user_id: user.id) }
       end.to change(BeanReview, :count).by 1
     end
   end
