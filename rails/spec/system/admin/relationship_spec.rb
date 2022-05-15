6e5a4a2009c736868e298e328b32a7b6f4bc3eb0
tomoron <tomoron326@outlook.jp>
2022-05-15 22:47:25 +0900

    test:admin relationship

---
+++ rails/spec/system/admin/relationship_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin relationship', type: :system do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:user1) { create(:user, username: 'test1', email: 'test1@example.com'}
+  let(:follow) { create(:relationship, user: user, follow: user1) }}
+
+  describe 'relationship' do
+    before do
+      admin_login(admin)
+      follow
+      visit admin_relationships_path
+    end
+
+    it 'displayed index' do
+      expect(page).to have_content 'フォロー'
+    end
+
+    it 'displayed show' do
+      visit admin_relationship_path(follow.id)
+      expect(page).to have_content user1.username
+      expect(page).to have_content user.username
+    end
+
+    it 'delete resource' do
+      expect do
+        visit admin_relationship_path(follow.id)
+        click_on 'フォロー を削除する'
+      end.to change(Relationship, :count).by(-1)
+    end
+  end
+end
