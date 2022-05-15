ac0871bd6b5332a40853703e2531af988f1bf25c
tomoron <tomoron326@outlook.jp>
2022-05-15 21:45:05 +0900

    test:bean_review.rb

---
+++ rails/spec/system/admin/bean_review.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin bean_review', type: :system do
+  let(:admin) { create(:admin) }
+  let(:user) { create(:user) }
+  let(:bean) { create(:bean) }
+  let(:bean_review) { create(:bean_review, user: user, bean: bean) }
+  let(:bean_review_like) { create(:like, user: user, liked_id: bean_review.id, type: 'BeanReviewLike') }
+
+  describe 'bean_review' do
+    before do
+      admin_login(admin)
+      bean_review_like
+      visit admin_bean_reviews_path
+    end
+
+    it 'displayed index' do
+      expect(page).to have_content 'コーヒー豆レビュー'
+    end
+
+    it 'displayed show' do
+      visit admin_bean_review_path(bean_review.id)
+      expect(page).to have_content bean_review.title
+      expect(page).to have_content user.username
+    end
+
+    # なぜかsubmitが操作できない
+
+    # it 'create resource' do
+    #   visit new_admin_bean_review_path
+    #   select bean.name, from: 'Bean'
+    #   select user.username, from: 'User'
+    #   fill_in '酸味', with: 1
+    #   fill_in '苦味', with: 2
+    #   fill_in 'コク', with: 3
+    #   fill_in '風味', with: 4
+    #   fill_in '甘み', with: 5
+    #   fill_in 'タイトル', with: 'testタイトル'
+    #   fill_in '投稿本文', with: 'testコンテンツ'
+    #   expect(page).to have_current_path new_admin_bean_review_path
+    #   expect(page).to have_content '取り消す'
+    #   # find("input[data-disable-with='コーヒー豆レビューを作成']").click
+    #   # expect(BeanReview.count).to eq 2
+    # end
+
+    it 'delete resource' do
+      expect do
+        visit admin_bean_review_path(bean_review.id)
+        click_on 'コーヒー豆レビュー を削除する'
+      end.to change(BeanReview, :count).by(-1)
+    end
+  end
+end
