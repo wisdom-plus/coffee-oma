c50d89a5c78508141aef357108f7977ad08b5895
tomoron <tomoron326@outlook.jp>
2022-05-15 22:39:02 +0900

    test:admin review

---
+++ rails/spec/system/admin/review_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin review', type: :system do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:product) { create(:product, user: user) }
+  let(:review) { create(:review, user: user, product: product) }
+  let(:product_reveiw_like) { create(:like, user: user, liked_id: review.id, type: 'ProductReviewLike') }
+
+  describe 'review' do
+    before do
+      admin_login(admin)
+      product_review_like
+      visit admin_reviews_path
+    end
+
+    it 'displayed index' do
+      expect(page).to have_content '商品レビュー'
+    end
+
+    it 'displayed' do
+      visit admin_review_path(review.id)
+      expect(page).to have_content review.title
+    end
+
+    it 'delete resource' do
+      expect do
+        visit admin_review_path(review.id)
+        click_on '商品レビュー を削除する'
+      end.to change(Review, :count).by(-1)
+    end
+  end
+end
