33a70d971bf72bc7b1aa4a0bb9048f3298dde962
tomoron <tomoron326@outlook.jp>
2022-05-17 12:30:14 +0900

    test:add admin report

---
+++ rails/spec/system/admin/report_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin report', type: :system do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:user1) { create(:user, email: 'test2@example.com', username: 'test2') }
+  let(:product) { create(:product, user: user) }
+  let(:bean) { create(:bean, user: user) }
+  let(:bean_review) { create(:bean_review, user: user, bean: bean) }
+  let(:review) { create(:review, user: user, product: product) }
+  let(:review1) { create(:review, user: user1, product: product) }
+  let(:review_report) { create(:report, user: user1, review: review) }
+  let(:bean_review_report) { create(:report, user: user1, review: bean_review) }
+
+  describe 'report' do
+    before do
+      admin_login(admin)
+      review_report
+      visit admin_reports_path
+    end
+
+    it 'displayed index' do
+      expect(page).to have_content '通報'
+    end
+
+    it 'displayed show(review)' do
+      visit admin_report_path(review_report.id)
+      expect(page).to have_content user1.username
+      expect(page).to have_content review_report.review.product.itemname
+    end
+
+    it 'displayed show(bean_review)' do
+      visit admin_report_path(bean_review_report.id)
+      expect(page).to have_content user1.username
+      expect(page).to have_content bean_review_report.review.bean.name
+    end
+
+    it 'delete resource' do
+      expect do
+        visit admin_report_path(review_report.id)
+        click_on '通報 を削除する'
+      end.to change(Report, :count).by(-1)
+    end
+  end
+end
