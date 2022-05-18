49b34c52ce1481ebaa7f82ec56c9bb4e50fb9165
tomoron <tomoron326@outlook.jp>
2022-05-18 10:23:03 +0900

    fix:let

--- rails/spec/requests/admin/bean_review_spec.rb
+++ rails/spec/requests/admin/bean_review_spec.rb
@@ -2,11 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Admin', type: :request do
   let(:admin) { create(:admin_user) }
-
   let_it_be(:user) { create(:user) }
   let_it_be(:bean) { create(:bean, user: user) }
-  let_it_be(:bean_review) { create(:bean_review, user: user, bean: bean) }
-  let_it_be(:recipe) { create(:recipe, bean_review: bean_review) }
 
   describe 'BeanReview' do
     before do
