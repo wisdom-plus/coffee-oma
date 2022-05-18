f47fd0edd1f141a18434154862e59f633e217bcc
tomoron <tomoron326@outlook.jp>
2022-05-18 14:00:59 +0900

    test:add request recipe

---
+++ rails/spec/requests/admin/recipe_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin', type: :request do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:bean) { create(:bean, user: user) }
+  let(:bean_review) { create(:bean_review, user: user, bean: bean) }
+
+
+  describe 'Recipe' do
+    before do
+      bean_review
+      sign_in admin
+    end
+
+    it 'created resource' do
+      expect do
+        post admin_recipes_path, params: { recipe: attributes_for(:recipe, bean_review_id: bean_review.id) }
+      end.to change(Recipe, :count).by 1
+    end
+  end
+end
