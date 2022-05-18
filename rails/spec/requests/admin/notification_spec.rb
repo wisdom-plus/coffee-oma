9a00321f74fd958da6cc0a510d3c00ff6a35b2dc
tomoron <tomoron326@outlook.jp>
2022-05-18 13:55:08 +0900

    test:add request notification

---
+++ rails/spec/requests/admin/notification_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin', type: :request do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:user1) { create(:user, email: 'test1@example.com', username: 'test2') }
+  let(:notification) { create(:notification, visitor: user, visited: user1, action: 'follow') }
+
+
+  describe 'Notification' do
+    before do
+      user
+      user1
+      sign_in admin
+    end
+
+    it 'created resource' do
+      expect do
+        post admin_notifications_path, params: { notification: attributes_for(:notification, visitor_id: user.id, visited: user1.id, action: 'follow') }
+      end.to change(Notification, :count).by 1
+    end
+  end
+end
