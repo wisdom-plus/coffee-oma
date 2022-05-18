c397f53c4fba17ec125b1dd2fedb936d4a053d79
tomoron <tomoron326@outlook.jp>
2022-05-18 11:08:09 +0900

    test:add request admin product_like

---
+++ rails/spec/requests/admin/product_like_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin', type: :request do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:product) { create(:product, user: user) }
+
+
+  describe 'BeanLike' do
+    before do
+      user
+      product
+      sign_in admin
+    end
+
+    it 'created resource' do
+      expect do
+        post admin_product_likes_path, params: { product_like: attributes_for(:like, user_id: user.id,liked_id: product.id,type: 'ProductLike') }
+      end.to change(Like, :count).by 1
+    end
+  end
+end
