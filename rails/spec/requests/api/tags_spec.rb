76b4e2b9ff526ec8516c137bc1b059736d1aefa8
tomoron <tomoron326@outlook.jp>
2022-05-18 17:05:52 +0900

    fix:rubocop

--- rails/spec/requests/api/tags_spec.rb
+++ rails/spec/requests/api/tags_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Api', type: :request do
   let(:user) { create(:user) }
 
-
   describe 'Tag' do
     it 'request success(keyword present)' do
       get api_v1_tags_path, params: { keyword: 'test' }
