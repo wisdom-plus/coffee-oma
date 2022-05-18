37d70e921eae716e148bae21585cdbeea3af6ffb
tomoron <tomoron326@outlook.jp>
2022-05-18 16:11:33 +0900

    test:add admin product created product

--- rails/spec/system/admin/product_spec.rb
+++ rails/spec/system/admin/product_spec.rb
@@ -19,6 +19,19 @@ RSpec.describe 'Admin product', type: :system do
       expect(page).to have_content '商品'
     end
 
+    it 'created product' do
+      expect do
+        visit new_admin_product_path
+        fill_in 'product_itemname', with: 'test_product'
+        fill_in 'product_itemprice', with: '100'
+        fill_in 'product_shopname', with: 'test_shop'
+        fill_in 'product_itemurl', with: 'https://example.com'
+        fill_in 'product_itemcaption', with: 'test_caption'
+        fill_in 'product_user_id', with: user.id
+        click_on '商品を作成'
+      end.to change(Product, :count).by(1)
+    end
+
     it 'displayed show' do
       visit admin_product_path(product.id)
       expect(page).to have_content product.itemname
