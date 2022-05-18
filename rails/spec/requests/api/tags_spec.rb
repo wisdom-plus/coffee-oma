00881630d85e200d3ea64fca8471d52c54532db0
tomoron <tomoron326@outlook.jp>
2022-05-18 17:05:17 +0900

    test:request api tags

---
+++ rails/spec/requests/api/tags_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Api', type: :request do
+  let(:user) { create(:user) }
+
+
+  describe 'Tag' do
+    it 'request success(keyword present)' do
+      get api_v1_tags_path, params: { keyword: 'test' }
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'request success(keyword nil)' do
+      get api_v1_tags_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
