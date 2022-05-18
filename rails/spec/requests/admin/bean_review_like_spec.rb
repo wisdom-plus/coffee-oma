2679d88b1d21fa25b1f877ea096cfaa0d7de9882
tomoron <tomoron326@outlook.jp>
2022-05-18 11:28:00 +0900

    test:add request admin bean_review_like

---
+++ rails/spec/requests/admin/bean_review_like_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin', type: :request do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  let(:bean) { create(:bean, user: user)}
+  let(:bean_review) { create(:bean_review, bean: bean, user: user)}
+
+
+  describe 'BeanReviewLike' do
+    before do
+      user
+      bean_review
+      sign_in admin
+    end
+
+    it 'created resource' do
+      expect do
+        post admin_bean_review_likes_path, params: { bean_review_like: attributes_for(:like, user_id: user.id,liked_id: bean_review.id,type: 'BeanReviewLike') }
+      end.to change(Like, :count).by 1
+    end
+  end
+end
