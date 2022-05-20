a56320b8d49b29570ac27eb89d05a979f1fb6157
tomoron <tomoron326@outlook.jp>
2022-05-20 21:40:38 +0900

    add:annottate

--- rails/spec/models/tag_spec.rb
+++ rails/spec/models/tag_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id             :integer          not null, primary key
+#  name           :string(255)
+#  taggings_count :integer          default(0)
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+# Indexes
+#
+#  index_tags_on_name  (name) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
