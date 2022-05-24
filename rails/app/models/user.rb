8fdaf066ca2608ffa6bba707bc9acf3842172c88
tomoron <tomoron326@outlook.jp>
2022-05-24 20:37:19 +0900

    rm:concering

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -25,34 +25,10 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
-  concerning :Follow do
-    included do
-      has_many :relationships, dependent: :destroy
-      has_many :followings, through: :relationships, source: :follow
-      has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy, inverse_of: 'user'
-      has_many :followers, through: :reverse_of_relationships, source: :user
-    end
-
-    def follow(other_user)
-      return if self == other_user
-
-      relationships.find_or_create_by(follow_id: other_user.id)
-    end
-
-    def unfollow(other_user_id)
-      relationship = relationships.find_by(follow_id: other_user_id)
-      relationship&.destroy
-    end
-
-    def following?(other_user)
-      followings.include?(other_user)
-    end
-
-    def follow_user(other_user)
-      relationships.find_by(follow_id: other_user.id)
-    end
-  end
-
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy, inverse_of: 'user'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   has_many :likes, dependent: :destroy
   has_many :bean_likes, dependent: :destroy
   has_many :product_likes, dependent: :destroy
