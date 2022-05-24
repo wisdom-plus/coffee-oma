7a918b9509b717c3fb8135530d6c1ffefab01dca
tomoron <tomoron326@outlook.jp>
2022-05-24 21:17:40 +0900

    fix:file name

---
+++ rails/app/models/users/follow.rb
@@ -0,0 +1,29 @@
+module Users::Follow
+  class Follow
+    def initialize(user,follower)
+      @user = user
+      @follower = follower
+    end
+
+    def follow
+      user.relationships.find_or_create_by(follow_id: follower.id)
+    end
+
+    def unfollow
+      relationship = user.relationships.find_by(follow_id: follower.id)
+      relationship&.destroy
+    end
+
+    def following?
+      user.followings.include?(follower)
+    end
+
+    def follow_user
+      relationships.find_by(follow_id: follower.id)
+    end
+
+    private
+
+    attr_reader :user, :follower
+  end
+end
