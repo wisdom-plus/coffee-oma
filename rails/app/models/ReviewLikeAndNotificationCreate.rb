1406e47d0504cf367c52e191c2ac8a19054038f6
tomoron <tomoron326@outlook.jp>
2022-05-26 21:23:20 +0900

    add:create

--- rails/app/models/ReviewLikeAndNotificationCreate.rb
+++ rails/app/models/ReviewLikeAndNotificationCreate.rb
@@ -1,9 +1,29 @@
 class ReviewLikeAndNotificationCreate
 # review_likeとnotificationを作成する
 
-  def initialize(user)
+  def initialize(user,type,liked_id)
     @user = user
+    @type = type
+    @liked_id = liked_id
+  end
+
+  def create
+    case type
+    when 'Review'
+      review_like = @user.product_review_likes.find_or_create_by(liked_id: liked_id)
+      review = Review.find(liked_id)
+    when 'BeanReview'
+      review_like = @user.bean_review_likes.find_or_create_by(liked_id: liked_id)
+      review = BeanReview.find(liked_id)
+    end
+    create_notification(@user,review.user_id)
+    return review_like
   end
 
 
+  private
+
+  def create_notification(like_id,user_id)
+    @user.active_notifications.new(like_id: like_id, visited_id: user_id, action: 'like')
+  end
 end
