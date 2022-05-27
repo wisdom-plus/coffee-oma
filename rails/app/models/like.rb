e7e66af7d97235caa191ea48e25dbf0234c0d563
tomoron <tomoron326@outlook.jp>
2022-05-27 20:54:02 +0900

    commentout:create_nitification

--- rails/app/models/like.rb
+++ rails/app/models/like.rb
@@ -20,10 +20,14 @@ class Like < ApplicationRecord
 
   scope :find_product_and_bean, ->(user_id) { where(["user_id = ? and type IN ('ProductLike','BeanLike')", user_id]).order(created_at: :DESC) }
 
-  def create_notification(current_user, user_id)
-    notification = current_user.create_like_notification(id, user_id, self.class.name)
-    notification.save
-  end
+  # テストで利用されていない
+  # :nocov:
+  # def create_notification(current_user, user_id)
+  #   notification = current_user.create_like_notification(id, user_id, self.class.name)
+  #   notification.save
+  # end
+  # :nocov:
+
 
   def self.product_and_bean_likes_includes(likes)
     preloader = ActiveRecord::Associations::Preloader.new
