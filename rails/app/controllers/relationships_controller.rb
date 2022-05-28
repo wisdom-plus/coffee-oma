cb7594c399a6bfefcc311382b7d312ff828154ff
tomoron <tomoron326@outlook.jp>
2022-05-28 20:19:13 +0900

    add:comment

--- rails/app/controllers/relationships_controller.rb
+++ rails/app/controllers/relationships_controller.rb
@@ -5,6 +5,7 @@ class RelationshipsController < ApplicationController
     @user = User.find_by(id: params[:follow_id])
     @follow = Follow.new(current_user, @user).follow
     @user.create_notification_follow(current_user)
+    # @userと@followが必要
     respond_to do |format|
       format.js
       format.html { redirect_to user_registration_path(@user) }
