1583656ed1beadd7f52d63c603aec5e4d4f16f96
tomoron <tomoron326@outlook.jp>
2022-05-29 20:19:54 +0900

    rm:unused method

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -56,23 +56,9 @@ class User < ApplicationRecord
     followings.include?(other_user)
   end
 
-  # def create_follow_notification(follower_id)
-  #   active_notifications.new(visited_id: follower_id, action: 'follow')
-  # end
-
   def create_message_notification(message_id, user_id)
     active_notifications.new(message_id: message_id, visited_id: user_id, action: 'message')
   end
-
-  # def create_notification_follow(current_user)
-  #   temp = Notification.follow_notification(current_user.id, id)
-  #   if temp.present?
-  #     temp.update(checked: false)
-  #   else
-  #     notification = current_user.create_follow_notification(id)
-  #     notification.save
-  #   end
-  # end
 
   def update_without_current_password(params, *options)
     params.delete(:current_password)
