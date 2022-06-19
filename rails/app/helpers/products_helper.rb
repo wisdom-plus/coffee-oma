491b42c752217e3bd57e06e9e031cf4f549b91c0
tomoron <tomoron326@outlook.jp>
2022-06-19 20:45:40 +0900

    pref:column_name

--- rails/app/helpers/products_helper.rb
+++ rails/app/helpers/products_helper.rb
@@ -1,7 +1,7 @@
 module ProductsHelper
   def item_url_link(product)
-    return if product.itemurl.empty?
+    return if product.url.empty?
 
-    link_to '商品を詳しく見る', product.itemurl.to_s, class: ' circular ui red button'
+    link_to '商品を詳しく見る', product.url.to_s, class: ' circular ui red button'
   end
 end
