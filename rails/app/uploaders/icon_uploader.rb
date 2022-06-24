6e10d8d1568293b390677dc8ed8baf2abd1dba6e
tomoron <tomoron326@outlook.jp>
2022-06-24 20:43:01 +0900

    add:minimagick

--- rails/app/uploaders/icon_uploader.rb
+++ rails/app/uploaders/icon_uploader.rb
@@ -1,7 +1,7 @@
 class IconUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -12,6 +12,8 @@ class IconUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
+
+  process resize_and_pad: [500, 350, '#ffffff']
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -56,4 +58,6 @@ class IconUploader < CarrierWave::Uploader::Base
   def filename
     original_filename
   end
+
+
 end
