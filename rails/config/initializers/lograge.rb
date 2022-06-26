5e5d7d8a7e1a8683ad6d48ced1ad7f944e4b2876
tomoron <tomoron326@outlook.jp>
2022-06-26 21:21:47 +0900

    add_config:formatter

--- rails/config/initializers/lograge.rb
+++ rails/config/initializers/lograge.rb
@@ -4,4 +4,6 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     {time: Time.now}
   end
+
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
 end
