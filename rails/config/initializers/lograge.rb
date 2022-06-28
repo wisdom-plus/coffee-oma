f385796c7e2219169976698955567c9f20f2b388
tomoron <tomoron326@outlook.jp>
2022-06-28 10:39:22 +0900

    fix:rubocop

--- rails/config/initializers/lograge.rb
+++ rails/config/initializers/lograge.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   config.lograge.base_controller_class = ['ActionController::API', 'ActionController::Base']
 
-  config.lograge.custom_options = lambda do |event|
-    {time: Time.now}
+  config.lograge.custom_options = ->(_event) do
+    { time: Time.zone.now }
   end
 
   config.lograge.formatter = Lograge::Formatters::Logstash.new
