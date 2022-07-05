eaeac7d3ee6e3e4bb4e18a37c0bc17acbbc8eca7
tomoron <tomoron326@outlook.jp>
2022-07-05 10:58:38 +0900

    change:belongs_to

--- rails/app/models/notification.rb
+++ rails/app/models/notification.rb
@@ -7,32 +7,24 @@
 #  checked    :boolean          default(FALSE), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  like_id    :bigint
-#  message_id :bigint
-#  visited_id :bigint
-#  visitor_id :bigint
+#  record_id  :bigint           not null
+#  user_id    :bigint
 #
 # Indexes
 #
 #  index_notifications_on_checked     (checked)
 #  index_notifications_on_created_at  (created_at)
-#  index_notifications_on_like_id     (like_id)
-#  index_notifications_on_message_id  (message_id)
-#  index_notifications_on_visited_id  (visited_id)
-#  index_notifications_on_visitor_id  (visitor_id)
+#  index_notifications_on_user_id     (user_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (like_id => likes.id)
-#  fk_rails_...  (message_id => messages.id)
-#  fk_rails_...  (visited_id => users.id)
-#  fk_rails_...  (visitor_id => users.id)
+#  fk_rails_...  (user_id => users.id)
 #
 class Notification < ApplicationRecord
+  belongs_to :user
   belongs_to :message, optional: true
   belongs_to :like, optional: true
-  belongs_to :visitor, class_name: 'User', optional: true
-  belongs_to :visited, class_name: 'User', optional: true
+  belongs_to :relationship,optional: true
 
   before_save :check_some_user?
 
