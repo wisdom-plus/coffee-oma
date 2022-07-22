0cbbf34fcb89c1efdf3fad3b42c5389d0326aedb
tomoron <tomoron326@outlook.jp>
2022-07-22 21:07:07 +0900

    fix:source_type

--- rails/app/controllers/notifications_controller.rb
+++ rails/app/controllers/notifications_controller.rb
@@ -3,11 +3,11 @@ class NotificationsController < ApplicationController
   after_action -> { Notification.checked_notifications(current_user.id) }
 
   def index
-    @notifications = current_user.notifications.includes(:like, :message,:relationships).limit(25).history_order
-    @follow_notifications = @notifications.type_filter('follow')
+    @notifications = current_user.notifications.limit(25).history_order
+    @follow_notifications = @notifications.type_filter('Relationship')
     @bean_review_like_notifications = @notifications.type_filter('BeanReviewLike')
     @product_review_like_notifications = @notifications.type_filter('ProductReviewLike')
-    @message_notifications = @notifications.type_filter('message')
+    @message_notifications = @notifications.type_filter('Message')
     notification_include(@notifications)
     review_like_include(@bean_review_like_notifications)
     bean_review_like_include(@product_review_like_notifications)
@@ -17,17 +17,17 @@ class NotificationsController < ApplicationController
 
     def notification_include(likes)
       preloader = ActiveRecord::Associations::Preloader.new
-      preloader.preload(likes.select { |i| i.action == 'BeanReviewLike' }, like: :bean_review)
-      preloader.preload(likes.select { |i| i.action == 'ProductReviewLike' }, like: :review)
+      preloader.preload(likes.select { |i| i.source_type == 'BeanReviewLike' }, like: :bean_review)
+      preloader.preload(likes.select { |i| i.source_type == 'ProductReviewLike' }, like: :review)
     end
 
     def review_like_include(likes)
       preloader = ActiveRecord::Associations::Preloader.new
-      preloader.preload(likes.select { |i| i.action == 'BeanReviewLike' }, like: :bean_review)
+      preloader.preload(likes.select { |i| i.source_type == 'BeanReviewLike' }, like: :bean_review)
     end
 
     def bean_review_like_include(likes)
       preloader = ActiveRecord::Associations::Preloader.new
-      preloader.preload(likes.select { |i| i.action == 'ProductReviewLike' }, like: :review)
+      preloader.preload(likes.select { |i| i.source_type == 'ProductReviewLike' }, like: :review)
     end
 end
