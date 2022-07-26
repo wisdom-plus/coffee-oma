bfa1a43353dcd3c845dd4237c355905c20b41507
tomoron <tomoron326@outlook.jp>
2022-07-26 20:57:29 +0900

    add:notifications

--- rails/spec/factories/notifications.rb
+++ rails/spec/factories/notifications.rb
@@ -23,6 +23,23 @@
 #
 FactoryBot.define do
   factory :notification do
-    association :visited, factory: :user
+    user
+    checked { false }
+
+    factory :product_review_like_notification do
+      source { product_review_like }
+    end
+
+    factory :bean_review_like_notification do
+      source { bean_review_like }
+    end
+
+    factory :message_notification do
+      source { message }
+    end
+
+    factory :follow_notification do
+      source { relationship}
+    end
   end
 end
