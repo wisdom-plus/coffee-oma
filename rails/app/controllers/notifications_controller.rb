701c285c1260a006efef1db33ce8b50e5fb941c1
tomoron <tomoron326@outlook.jp>
2022-07-26 14:15:07 +0900

    fix:preload

--- rails/app/controllers/notifications_controller.rb
+++ rails/app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
   after_action -> { Notification.checked_notifications(current_user.id) }
 
   def index
-    @notifications = current_user.notifications.limit(25).history_order
+    @notifications = current_user.notifications.includes(:source).limit(25).history_order
     @follow_notifications = @notifications.type_filter('Relationship')
     @like_notifications = @notifications.type_filter('Like')
     @message_notifications = @notifications.type_filter('Message')
@@ -16,13 +16,13 @@ class NotificationsController < ApplicationController
 
     def notification_include(likes)
       preloader = ActiveRecord::Associations::Preloader.new
-      preloader.preload(likes.select { |i| i.source_type == 'BeanReviewLike' }, like: :bean_review)
-      preloader.preload(likes.select { |i| i.source_type == 'ProductReviewLike' }, like: :review)
+      preloader.preload(likes.select { |i| i.source.class.name == 'BeanReviewLike' }, source: [:bean_review,:user])
+      preloader.preload(likes.select { |i| i.source.class.name == 'ProductReviewLike' }, source: [:review,:user])
     end
 
     def review_like_include(likes)
       preloader = ActiveRecord::Associations::Preloader.new
-      preloader.preload(likes.select { |i| i.source_type == 'BeanReviewLike' }, like: :bean_review)
+      preloader.preload(likes.select { |i| i.source == 'BeanReviewLike' }, like: :bean_review)
     end
 
     def bean_review_like_include(likes)
