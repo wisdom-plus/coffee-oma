716d81f970dc548d7b0b1be70cf8345144edb515
tomoron <tomoron326@outlook.jp>
2022-07-26 15:33:55 +0900

    add:includes user

--- rails/app/controllers/notifications_controller.rb
+++ rails/app/controllers/notifications_controller.rb
@@ -3,11 +3,12 @@ class NotificationsController < ApplicationController
   after_action -> { Notification.checked_notifications(current_user.id) }
 
   def index
-    @notifications = current_user.notifications.includes(:source).limit(25).history_order
+    @notifications = current_user.notifications.includes(source: :user).limit(25).history_order
     @follow_notifications = @notifications.type_filter('Relationship')
     @like_notifications = @notifications.type_filter('Like')
     @message_notifications = @notifications.type_filter('Message')
     notification_include(@notifications)
+    session[:checked] = false if session[:checked]
   end
 
   private
