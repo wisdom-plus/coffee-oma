364a0c81455a8b5e8bc59eaa77d95fffb50ac341
tomoron <tomoron326@outlook.jp>
2022-08-03 20:43:28 +0900

    change:migrate up down

--- rails/db/migrate/20220703115229_remove_column_to_notification.rb
+++ rails/db/migrate/20220703115229_remove_column_to_notification.rb
@@ -1,5 +1,7 @@
 class RemoveColumnToNotification < ActiveRecord::Migration[6.1]
-  def change
+
+
+  def up
     remove_reference :notifications, :like
     remove_reference :notifications, :message
     remove_reference :notifications, :visitor
@@ -8,4 +10,23 @@ class RemoveColumnToNotification < ActiveRecord::Migration[6.1]
     add_reference :notifications, :user, foreign_key: true
     add_reference :notifications, :source, polymorphic: true
   end
+
+  def down
+    add_reference :notifications, :like, foreign_key: true
+    add_reference :notifications, :message, foreign_key: true
+    add_reference :notifications, :visitor, foreign_key: true
+    add_reference :notifications, :visited, foreign_key: true
+    remove_reference :notifications, :user
+    remove_reference :notifications, :source
+  end
+
+  # def change
+  #   remove_reference :notifications, :like
+  #   remove_reference :notifications, :message
+  #   remove_reference :notifications, :visitor
+  #   remove_reference :notifications, :visited
+  #   remove_column :notifications, :action, :string
+  #   add_reference :notifications, :user, foreign_key: true
+  #   add_reference :notifications, :source, polymorphic: true
+  # end
 end
