8750925fefa4e4065ade454ed84f38218a0a3048
tomoron <tomoron326@outlook.jp>
2022-08-10 16:41:46 +0900

    fix:to fullhash

--- rails/config/webpack/development.js
+++ rails/config/webpack/development.js
@@ -3,6 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 const environment = require("./environment");
 
 const config = environment.toWebpackConfig();
-config.output.filename = "js/[name]-[hash].js";
+config.output.filename = "js/[name]-[fullhash].js";
 
 module.exports = config;
