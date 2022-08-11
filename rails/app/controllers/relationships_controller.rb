25e6584bbe3e80c4d54fca376fa85aa827fe868e
tomoron <tomoron326@outlook.jp>
2022-08-11 15:31:15 +0900

    add:flash define

--- rails/app/controllers/relationships_controller.rb
+++ rails/app/controllers/relationships_controller.rb
@@ -4,6 +4,11 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by(id: params[:follow_id])
     @follow = FollowAndNotificationCreate.new(current_user, @user).create
+    if @follow.save
+      flash[:notice] = "#{@user.username}をフォローしました"
+    else
+      flash[:alert] = "フォローに失敗しました"
+    end
     # @userと@followが必要
     respond_to do |format|
       format.js
@@ -14,6 +19,11 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = User.find_by(id: params[:follow_id])
     @follow = Follow.new(current_user, @user).unfollow
+    if @follow.destroy
+      flash[:notice] = "#{@user.username}のフォローを解除しました"
+    else
+      flash[:alert] = "フォロー解除に失敗しました"
+    end
     respond_to do |format|
       format.js
       format.html { redirect_to user_registration_path(@user) }
