3a9d3faf1bd1d97e09b121c10bb2c8b0e151e8b5
tomoron <tomoron326@outlook.jp>
2022-08-16 19:13:29 +0900

    add:room new spec

--- rails/spec/requests/rooms_spec.rb
+++ rails/spec/requests/rooms_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Rooms', type: :request do
   let_it_be(:user) { create(:user) }
   let_it_be(:user1) { create(:user, email: 'test1@example.com', username: 'test2') }
+  let_it_be(:user2) { create(:user, email: 'test2@example.com', username: 'test3') }
+  let(:follow) { create(:relationship, user: user, follow: user1) }
+  let(:follow2) { create(:relationship, user: user, follow: user2) }
   let(:room) { create(:room, participant1: user, participant2: user1) }
   let(:message) { create(:message, user: user, room: room) }
 
@@ -16,6 +19,25 @@ RSpec.describe 'Rooms', type: :request do
 
     it 'request success' do
       get rooms_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET new' do
+    before do
+      user.confirm
+      sign_in user
+      follow
+      follow2
+    end
+
+    it 'request success' do
+      get new_room_path
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'serach success' do
+      get new_room_path, params: {q: user2.username}
       expect(response).to have_http_status(:ok)
     end
   end
