8926ef384cebd649f1b0fe8c0581f5aff348c0ca
tomoron <tomoron326@outlook.jp>
2022-09-14 17:25:18 +0900

    port

--- rails/vite.config.ts
+++ rails/vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
     RubyPlugin(),
     FullReload(["config/routes.rb", "app/views/**/*"], { delay: 100 }),
   ],
+  server: {
+    hmr: {
+      port: 3036,
+      protocol: "ws",
+      host: "localhost",
+      clientPort: 3036,
+    },
+    port: 3036,
+  },
 });
