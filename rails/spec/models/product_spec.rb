5c2698610916535998d90188d417e7077e883d0b
tomoron <tomoron326@outlook.jp>
2022-09-27 15:38:33 +0900

    spec:product model

--- rails/spec/models/product_spec.rb
+++ rails/spec/models/product_spec.rb
@@ -25,4 +25,19 @@ RSpec.describe Product, type: :model do
       expect(product_build.valid?).to eq false
     end
   end
+
+  it "sort likes counts" do
+    product1 = create(:product, likes_count: 1, user: user)
+    product2 = create(:product, likes_count: 2, user: user)
+    product3 = create(:product, likes_count: 10, user: user)
+    expect(Product.sort_by_likes_count).to eq [product3, product2, product1]
+  end
+
+  it "search product name for keywords" do
+    product1 = create(:product, name: "コーヒー ドリッパー", user: user)
+    product2 = create(:product, name: "コーヒー サーバー", user: user)
+    product3 = create(:product, name: "コーヒー カップ", user: user)
+    expect(Product.keywords_search(name_cont: "ドリッパー").result).to eq [product1]
+    expect(Product.keywords_search(name_cont: "コーヒー").result).to eq [product1, product2, product3]
+  end
 end
