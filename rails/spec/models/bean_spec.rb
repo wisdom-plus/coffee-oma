8e352f3dcfc8174c60cb796d1eac3f4a9f7b65db
tomoron <tomoron326@outlook.jp>
2022-09-28 14:54:10 +0900

    spec:model bean spec

---
+++ rails/spec/models/bean_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Bean, type: :model do
+  let(:user ) { create(:user) }
+  let(:bean) { create(:bean,user: user) }
+  let(:bean_build) { build(:bean) }
+
+  it "is valid with a name" do
+    bean_build.name = ''
+    expect(bean_build.valid?).to be false
+  end
+
+  it "is valid with a country" do
+    bean_build.country = ''
+    expect(bean_build.valid?).to be false
+  end
+
+  it "is valid with a roast" do
+    bean_build.roast = ''
+    expect(bean_build.valid?).to be false
+  end
+
+  it "search bean for keyword" do
+    bean1 = create(:bean, name: "tokyo blend",user: user)
+    bean2 = create(:bean, name: "kyoto blend",user: user)
+    bean3 = create(:bean, name: "my blend",user: user)
+    expect(Bean.keywords_search(name_cont: "blend").result).to eq [bean1, bean2, bean3]
+    expect(Bean.keywords_search(name_cont: "kyoto").result).to eq [bean2]
+  end
+
+end
