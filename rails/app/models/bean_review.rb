4578e8e36f9b7bf72c441a74aa8cb0df9beee63d
tomoron <tomoron326@outlook.jp>
2022-09-29 13:41:21 +0900

    rm:method

--- rails/app/models/bean_review.rb
+++ rails/app/models/bean_review.rb
@@ -44,10 +44,6 @@ class BeanReview < ApplicationRecord
                       }
   scope :exclude_reviews, ->(bean_id, user_id) { ExcludeReportedBeanReviewsQuery.call(bean_id, user_id) }
 
-  # 利用しているかもしれないので、削除しない
-  # def like_record(liker_id)
-  #   bean_review_likes.find_by(user_id: liker_id)
-  # end
 
   def self.show_review(bean_id)
     accociated_review(bean_id).includes([:user], [:recipe])
