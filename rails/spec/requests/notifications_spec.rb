044dca32daaf558390fe346f3d7ae297fa987cb1
tomoron <tomoron326@outlook.jp>
2022-10-03 20:24:00 +0900

    pref:request notification

--- rails/spec/requests/notifications_spec.rb
+++ rails/spec/requests/notifications_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Notifications', type: :request do
-  let_it_be(:user) { create(:user) }
-  let_it_be(:user1) { create(:user, email: 'test1@example.com', username: 'test2') }
-  let_it_be(:follow) { create(:relationship, user: user, follow: user) }
-  let_it_be(:notification) { create(:notification, source: follow, user: user) }
+  let(:user) { create(:user) }
+  let(:user1) { create(:user, :other_user) }
+  let(:follow) { create(:relationship, user: user, follow: user) }
+  let(:notification) { create(:notification, source: follow, user: user) }
 
   describe 'GET /index' do
-    before do
+    it 'request success' do
       user.confirm
       sign_in user
-    end
-
-    it 'request success' do
       get notifications_path
       expect(response).to have_http_status(:ok)
     end
