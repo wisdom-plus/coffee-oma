0351f0e28c656a810baa35e2bad400bbb863a89f
tomoron <tomoron326@outlook.jp>
2022-10-09 15:06:27 +0900

    pref:admin dashboard system spec

--- rails/spec/system/admin/dashboard_spec.rb
+++ rails/spec/system/admin/dashboard_spec.rb
@@ -1,35 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin dashboard', type: :system do
-  let(:admin) { create(:admin_user) }
+  let!(:admin) { create(:admin_user) }
 
   describe 'dashboard' do
     context 'when logged in' do
-      before do
+      it 'display dashborad' do
         admin_login(admin)
         visit admin_root_path
-      end
-
-      it 'display dashborad' do
         expect(page).to have_content 'ダッシュボード'
         expect(page).to have_current_path admin_root_path
       end
     end
 
-    context 'when not logged in' do
+    context 'when not logged in after sign in page' do
       it 'login success' do
         visit new_admin_user_session_path
         fill_in 'メールアドレス', with: admin.email
         fill_in 'パスワード', with: admin.password
         click_button 'ログイン'
         expect(page).to have_content 'ログインしました。'
-      end
-
-      it 'after sign in page' do
-        visit new_admin_user_session_path
-        fill_in 'メールアドレス', with: admin.email
-        fill_in 'パスワード', with: admin.password
-        click_button 'ログイン'
         expect(page).to have_current_path admin_root_path
       end
     end
