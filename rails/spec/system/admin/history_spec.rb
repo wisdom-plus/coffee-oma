8e908dd1073d76e846c798624086e4553a7521b8
tomoron <tomoron326@outlook.jp>
2022-10-09 15:11:27 +0900

    pref:admin history system spec

--- rails/spec/system/admin/history_spec.rb
+++ rails/spec/system/admin/history_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe 'Admin history', type: :system do
   let(:user) { create(:user) }
   let(:product) { create(:product, user: user) }
   let(:bean) { create(:bean, user: user) }
-  let(:history1) { create(:history, product: product, user: user) }
-  let(:history2) { create(:history, bean: bean, user: user) }
+  let!(:history1) { create(:history, product: product, user: user) }
+  let!(:history2) { create(:history, bean: bean, user: user) }
 
   describe 'history' do
     before do
       admin_login(admin)
-      history1
-      history2
       visit admin_histories_path
     end
 
