92b2c8c4d0f910a9e0f8ffc6dff543ca97fd3e66
tomoron <tomoron326@outlook.jp>
2022-10-10 19:28:34 +0900

    add:image_attachment

---
+++ rails/spec/factories/image_attachments.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: image_attachments
+#
+#  id             :bigint           not null, primary key
+#  attachment     :string(255)
+#  imageable_type :string(255)      not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  imageable_id   :bigint           not null
+#
+# Indexes
+#
+#  index_image_attachments_on_imageable  (imageable_type,imageable_id)
+#
+FactoryBot.define do
+  factory :image_attachment do
+    
+  end
+end
