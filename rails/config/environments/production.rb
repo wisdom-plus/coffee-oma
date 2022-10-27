143131688a4b2ff6af43395f9a5b10a88a0fac03
tomoron <tomoron326@outlook.jp>
2022-10-27 20:09:59 +0900

    rm:sprocket files

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -25,10 +25,10 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
