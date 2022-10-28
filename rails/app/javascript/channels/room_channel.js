1092c51514b6803e380e663c108c2f082fbe5a51
tomoron <tomoron326@outlook.jp>
2022-10-28 20:42:35 +0900

    fix:jquery

--- rails/app/javascript/channels/room_channel.js
+++ rails/app/javascript/channels/room_channel.js
@@ -1,29 +1,33 @@
-import consumer from "./consumer"
-$(document).on('turbolinks:load', function () {
-  if ($('#room').length) {
-    const chatChannel = consumer.subscriptions.create({ channel: "RoomChannel", room_id: $('#room').data('room_id') }, {
-      connected() {
-        // Called when the subscription is ready for use on the server
-      },
+import consumer from "./consumer";
+import $ from "jquery";
 
-      disconnected() {
-        // Called when the subscription has been terminated by the server
-      },
+$(document).on("turbolinks:load", function () {
+  if ($("#room").length) {
+    const chatChannel = consumer.subscriptions.create(
+      { channel: "RoomChannel", room_id: $("#room").data("room_id") },
+      {
+        connected() {
+          // Called when the subscription is ready for use on the server
+        },
 
-      received(data) {
-        const element = document.getElementById('room');
-        if (element.dataset.user_id == data.user_id) {
-          const html = this.createspeakR(data);
-          element.insertAdjacentHTML('beforeend', html);
-        } else {
-          const html = this.createspeakL(data);
-          element.insertAdjacentHTML('beforeend', html);
-        };
-        document.getElementById('message_message').value = ''
-      },
+        disconnected() {
+          // Called when the subscription has been terminated by the server
+        },
 
-      createspeakR(data) {
-        return `
+        received(data) {
+          const element = document.getElementById("room");
+          if (element.dataset.user_id == data.user_id) {
+            const html = this.createspeakR(data);
+            element.insertAdjacentHTML("beforeend", html);
+          } else {
+            const html = this.createspeakL(data);
+            element.insertAdjacentHTML("beforeend", html);
+          }
+          document.getElementById("message_message").value = "";
+        },
+
+        createspeakR(data) {
+          return `
           <div class="balloon_r">
             <div class="faceicon">
               <img class="ui circular image" src=${data.iconurl}>
@@ -37,10 +41,10 @@ $(document).on('turbolinks:load', function () {
               </div>
             </div>
           </div>
-        `
-      },
-      createspeakL(data) {
-        return `
+        `;
+        },
+        createspeakL(data) {
+          return `
           <div class="balloon_l">
             <div class="faceicon">
               <img class="ui circular image" src=${data.iconurl}>
@@ -54,13 +58,13 @@ $(document).on('turbolinks:load', function () {
               <span class="date">${data.time}</span>
             </div>
           </div>
-        `
+        `;
+        },
       }
-    });
+    );
     this.subscription = chatChannel;
-  }
-  else if (this.subscription) {
+  } else if (this.subscription) {
     consumer.subscriptions.remove(this.subscription);
     delete this.subscription;
-  };
+  }
 });
