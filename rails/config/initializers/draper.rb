3d64052c9370cd70c2180d268bad438c2a0305d0
tomoron <tomoron326@outlook.jp>
2022-11-08 20:08:29 +0900

    pref:paginate decorate

---
+++ rails/config/initializers/draper.rb
@@ -0,0 +1,7 @@
+Draper::CollectionDecorator.delegate :current_page,
+                                     :total_pages,
+                                     :limit_value,
+                                     :entry_name,
+                                     :total_count,
+                                     :num_pages,
+                                     :offset_value
