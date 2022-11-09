d8d00b50369851a43ba3e05a2b0b65034a726c58
tomoron <tomoron326@outlook.jp>
2022-11-09 20:37:14 +0900

    add:review decorator

---
+++ rails/app/decorators/review_decorator.rb
@@ -0,0 +1,17 @@
+class ReviewDecorator < ApplicationDecorator
+  delegate_all
+
+  delegate :name, to: :product, prefix: true
+
+  def user_icon
+    user.icon.url
+  end
+
+  def user_name
+    user.username
+  end
+
+  def product_image
+    product.images[0].url
+  end
+end
