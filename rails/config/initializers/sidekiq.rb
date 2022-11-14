008062f5bd73cc8403889ba5d73ad6916c0f0a1f
tomoron <tomoron326@outlook.jp>
2022-11-14 21:21:37 +0900

    config:sidekiq

---
+++ rails/config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+require 'sidekiq/web'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'], timeout: 2 }
+end
