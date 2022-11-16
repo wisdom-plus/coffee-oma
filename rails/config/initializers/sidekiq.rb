b7c95914a630ff8ca41fef19c943157412269218
tomoron <tomoron326@outlook.jp>
2022-11-16 21:03:23 +0900

    job adapter test

--- rails/config/initializers/sidekiq.rb
+++ rails/config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV.fetch('REDIS_URL', nil) }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], timeout: 2 }
+  config.redis = { url: ENV.fetch('REDIS_URL', nil), timeout: 2 }
 end
