675b232797ad1d9db51e02402cc9c4f4e4011055
tomoron <tomoron326@outlook.jp>
2022-11-25 21:38:29 +0900

    add: review_form component

---
+++ rails/app/components/review_form/component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ReviewForm::Component < ViewComponent::Base
+  def initialize(review:, product_id:, current_user:)
+    @review = review
+    @product_id = product_id
+    @current_user = current_user
+  end
+
+  def user_signed_in?
+    @current_user.present?
+  end
+end
