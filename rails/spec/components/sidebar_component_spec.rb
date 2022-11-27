ff93b7f77d2ab273381b052dd08a0d405a33a00b
tomoron <tomoron326@outlook.jp>
2022-11-27 13:06:41 +0900

    test:menu_component

---
+++ rails/spec/components/sidebar_component_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Sidebar::Component, type: :component do
+  let(:user) { create(:user) }
+
+  it 'render the sidebar(not login)' do
+    render_inline(described_class.new(current_user: nil))
+
+    expect(page).to have_text 'ログイン'
+  end
+
+  it 'render the sidebar(login)' do
+    sign_in(user)
+    render_inline(described_class.new(current_user: user))
+
+    expect(page).to have_text 'ログアウト'
+  end
+end
