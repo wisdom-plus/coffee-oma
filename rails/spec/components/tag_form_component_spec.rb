7dc32d9857e655c09104944ac0a58948566204fc
tomoron <tomoron326@outlook.jp>
2022-11-27 18:22:01 +0900

    test:tag form component

---
+++ rails/spec/components/tag_form_component_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TagForm::Component, type: :component do
+  let(:user) { create(:user) }
+  let(:product) { create(:product, user: user) }
+  let(:bean) { create(:bean, user: user) }
+
+  it 'renders a form(product)' do
+    tag = product.tag_counts_on(:tags)
+    render_inline(described_class.new(tags: tag, target: product.decorate, current_user: nil))
+
+    expect(page).to have_css '.tag_link'
+    expect(page).to have_no_css '#accordion'
+  end
+
+  it 'renders a form(product, signed)' do
+    tag = product.tag_counts_on(:tags)
+    render_inline(described_class.new(tags: tag, target: product.decorate, current_user: user))
+
+    expect(page).to have_css '.tag_link'
+    expect(page).to have_css '#accordion'
+  end
+
+  it 'renders a form(bean)' do
+    tag = bean.tag_counts_on(:tags)
+    render_inline(described_class.new(tags: tag, target: bean.decorate, current_user: nil))
+
+    expect(page).to have_css '.tag_link'
+    expect(page).to have_no_css '#accordion'
+  end
+
+  it 'renders a form(bean , signed)' do
+    tag = bean.tag_counts_on(:tags)
+    render_inline(described_class.new(tags: tag, target: bean.decorate, current_user: user))
+
+    expect(page).to have_css '.tag_link'
+    expect(page).to have_css '#accordion'
+  end
+end
