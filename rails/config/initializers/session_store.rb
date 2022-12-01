34be22ff1433068fe3611d91dfdeba020cb5f2e6
tomoron <tomoron326@outlook.jp>
2022-12-01 20:10:07 +0900

    remove:secure

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -1,6 +1,8 @@
-# Rails.application.config.session_store :redis_store,
-#                                        servers: [ENV.fetch('REDIS_URL', nil)],
-#                                        expire_after: 1.day,
-#                                        key: "_#{Rails.application.class.module_parent_name.downcase}_session",
-#                                        threadsafe: true,
-#                                        secure: true
+secure = Rails.env.production?
+
+Rails.application.config.session_store :redis_store,
+                                       servers: [ENV.fetch('REDIS_URL', nil)],
+                                       expire_after: 1.day,
+                                       key: "_#{Rails.application.class.module_parent_name.downcase}_session",
+                                       threadsafe: true,
+                                       secure: secure
