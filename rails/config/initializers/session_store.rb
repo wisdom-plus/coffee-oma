0cec5b9f098cb0c9dc5deae450e77bb813348a0f
tomoron <tomoron326@outlook.jp>
2022-12-02 21:01:06 +0900

    pref:session_tore

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 secure = Rails.env.production?
 
 Rails.application.config.session_store :redis_store,
-                                       servers: [ENV.fetch('REDIS_URL', nil)],
+                                       servers: ["#{ENV.fetch('REDIS_URL', nil)}/0/session"],
                                        expire_after: 1.day,
                                        key: "_#{Rails.application.class.module_parent_name.downcase}_session",
                                        threadsafe: true,
