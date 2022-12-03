1f8c08216f8aa2950a28c04c4dda98c22e5b8e89
tomoron <tomoron326@outlook.jp>
2022-12-03 16:30:43 +0900

    fix:test cookie_store

--- rails/config/initializers/session_store.rb
+++ rails/config/initializers/session_store.rb
@@ -6,3 +6,7 @@ Rails.application.config.session_store :redis_store,
                                        key: "_#{Rails.application.class.module_parent_name.downcase}_session",
                                        threadsafe: true,
                                        secure: secure
+
+if Rails.env.test?
+  Rails.application.config.session_store :cookie_store, key: "_#{Rails.application.class.module_parent_name.downcase}_session"
+end
