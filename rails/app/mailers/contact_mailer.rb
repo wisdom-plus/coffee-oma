74ed0f47be4eb1e3c2e1f013880fba5d64aa7e22
tomoron <tomoron326@outlook.jp>
2022-12-09 20:35:01 +0900

    fix:config/sidekiq

--- rails/app/mailers/contact_mailer.rb
+++ rails/app/mailers/contact_mailer.rb
@@ -1,17 +1,15 @@
 class ContactMailer < ApplicationMailer
-  default from: 'noreply@coffee-oma.com'
-
   def contact_mail
     @contact = params[:contact]
     mail(
-      to: ENV.fetch('MY_MAILADRESS', nil)
+      to: ENV.fetch('MY_MAILADRESS', subject: '問い合わせメール')
     )
   end
 
   def reply_mail
     @contact = params[:contact]
     mail(
-      to: @contact.email
+      to: @contact.email, subject: '問い合わせ'
     )
   end
 end
