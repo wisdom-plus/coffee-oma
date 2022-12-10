62eb42e7c9bcf2ba78f3a36a246d5d076a6460e0
tomoron <tomoron326@outlook.jp>
2022-12-10 13:51:03 +0900

    fix:rubocop

--- rails/app/mailers/contact_mailer.rb
+++ rails/app/mailers/contact_mailer.rb
@@ -2,14 +2,14 @@ class ContactMailer < ApplicationMailer
   def contact_mail
     @contact = params[:contact]
     mail(
-      to: ENV.fetch('MY_MAILADRESS', subject: '問い合わせメール')
+      to: ENV.fetch('MY_MAILADRESS')
     )
   end
 
   def reply_mail
     @contact = params[:contact]
     mail(
-      to: @contact.email, subject: '問い合わせ'
+      to: @contact.email
     )
   end
 end
