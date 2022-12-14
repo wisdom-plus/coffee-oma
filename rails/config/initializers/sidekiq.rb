38b4709056fbc41f47c42a9fc4e5dc2d3052c9f6
tomoron <tomoron326@outlook.jp>
2022-12-14 20:24:41 +0900

    config:sidekiq-cron

--- rails/config/initializers/sidekiq.rb
+++ rails/config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
 require 'sidekiq/web'
+require 'sidekiq/cron/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', nil) }
+  config.on(:startup) do
+    schedule_file = 'config/schedule.yml'
+
+    if File.exist?(schedule_file)
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
