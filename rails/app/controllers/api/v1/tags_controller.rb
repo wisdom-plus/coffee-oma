7c2b3f2a288921d1335eb9e85b339f26c2ff52e0
tomoron <tomoron326@outlook.jp>
2022-12-17 17:46:38 +0900

    fix:tag keyword nil

--- rails/app/controllers/api/v1/tags_controller.rb
+++ rails/app/controllers/api/v1/tags_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::TagsController < ApplicationController
   def index
-    @tags = Tag.tag_search(params[:keyword]) if params[:keyword].present?
+    return nil unless params[:keyword].present?
+
+    @tags = Tag.tag_search(params[:keyword])
+    unless @tags.present?
+      @tags = [Tag.new(name: params[:keyword])]
+    end
   end
 end
