d7467d0714729167a651b760375623eb2d7f8f99
tomoron <tomoron326@outlook.jp>
2023-01-01 13:55:09 +0900

    fix:system spec

--- rails/spec/system/registrations_spec.rb
+++ rails/spec/system/registrations_spec.rb
@@ -92,6 +92,7 @@ RSpec.describe 'Registrations', type: :system, js: true do
     it 'when get login delete fail' do
       gest_login
       visit edit_user_registration_path
+      expect(page).to have_current_path '/users/edit'
       find('.delete-accordion').click
       find_by_id('modal-button').click
       click_on 'delete_button'
