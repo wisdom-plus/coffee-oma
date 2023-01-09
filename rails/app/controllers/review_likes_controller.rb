1546b9be4c6ea0f10e20d651559723aa407dbd02
tomoron <tomoron326@outlook.jp>
2023-01-09 17:00:48 +0900

    pref:review_like_controller

--- rails/app/controllers/review_likes_controller.rb
+++ rails/app/controllers/review_likes_controller.rb
@@ -8,33 +8,39 @@ class ReviewLikesController < ApplicationController
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream:
-          turbo_stream.replace(
-            "review_like_button_#{@review.id}",
-            ReviewLikeButton::Component.new(review: @review, review_like: @review_like).render_in(view_context)
-          )
+          turbo_stream_replace(@review, @review_like)
       end
       format.html { redirect_to product_path(@review), status: :see_other }
     end
   end
 
   def destroy
-    case params[:type]
-    when 'ProductReviewLike'
-      review_like = current_user.product_review_likes.find_by(id: params[:id])
-    when 'BeanReviewLike'
-      review_like = current_user.bean_review_likes.find_by(id: params[:id])
-    end
+    review_like = get_review_like(params[:type], params[:id])
     review_like&.destroy unless review_like.nil?
     @review = review_like.accociated_review.decorate
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream:
-          turbo_stream.replace(
-            "review_like_button_#{@review.id}",
-            ReviewLikeButton::Component.new(review: @review, review_like: nil).render_in(view_context)
-          )
+          turbo_stream_replace(@review)
       end
       format.html { redirect_to product_path(@review), status: :see_other }
     end
   end
+
+  private
+
+    def get_review_like(type, params_id)
+      if type == 'ProductReviewLike'
+        current_user.product_review_likes.find_by(id: params_id)
+      else
+        current_user.bean_review_likes.find_by(id: params_id)
+      end
+    end
+
+    def turbo_stream_replace(review, review_like = nil)
+      turbo_stream.replace(
+        "review_like_button_#{review.id}",
+        ReviewLikeButton::Component.new(review: review, review_like: review_like).render_in(view_context)
+      )
+    end
 end
