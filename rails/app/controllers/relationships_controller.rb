8ab1b8bf11d467a03c87584d340d4c4695818414
tomoron <tomoron326@outlook.jp>
2023-01-09 17:25:48 +0900

    pref:relationship controller

--- rails/app/controllers/relationships_controller.rb
+++ rails/app/controllers/relationships_controller.rb
@@ -5,13 +5,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by(id: params[:follow_id])
     @follow = FollowAndNotificationCreate.new(current_user, @user).create
-    if @follow.save
-      flash[:notice] = "#{@user.username}をフォローしました"
-    else
-      # :nocov:
-      flash[:alert] = t('.alert')
-      # :nocov:
-    end
+    create_flash(@follow, @user)
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
@@ -26,13 +20,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = User.find_by(id: params[:follow_id])
     @follow = Follow.new(current_user, @user).unfollow
-    if @follow.destroy
-      flash[:notice] = "#{@user.username}のフォローを解除しました"
-    else
-      # :nocov:
-      flash[:alert] = t('.alert')
-      # :nocov:
-    end
+    destroy_flash(@follow, @user)
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
@@ -43,4 +31,28 @@ class RelationshipsController < ApplicationController
       format.html { redirect_to user_registration_path(@user), status: :see_other }
     end
   end
+
+  private
+
+    def create_flash(follow, user)
+      if follow.save
+        flash[:notice] = "#{user.username}をフォローしました"
+      else
+        # :nocov:
+        flash[:alert] = t('.alert')
+        redirect_to user_registration_path(user), status: :see_other
+        # :nocov:
+      end
+    end
+
+    def destroy_flash(follow, user)
+      if follow.destroy
+        flash[:notice] = "#{user.username}のフォローを解除しました"
+      else
+        # :nocov:
+        flash[:alert] = t('.alert')
+        redirect_ot user_registration_path(user), status: :see_other
+        # :nocov:
+      end
+    end
 end
