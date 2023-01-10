8603c57d829f268e7a18ec3a513fac823d7ac35f
tomoron <tomoron326@outlook.jp>
2023-01-10 21:32:22 +0900

    remove:turbo redirect

--- rails/app/controllers/tags_controller.rb
+++ rails/app/controllers/tags_controller.rb
@@ -8,7 +8,7 @@ class TagsController < ApplicationController
     @target.tag_list = params[:tag_list].split(',')
     @target.save
     @tags = @target.tag_counts_on(:tags)
-    flash[:notice] = t('.notice')
+    flash.now[:notice] = t('.notice')
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: [
@@ -20,23 +20,23 @@ class TagsController < ApplicationController
     end
   end
 
-  def update
-    @target = get_target(params[:type], params[:id])
+  # def update
+  #   @target = get_target(params[:type], params[:id])
 
-    @target.tag_list = params[:tag_list].split(',')
-    @target.save
-    @tags = @target.tag_counts_on(:tags)
-    flash[:notice] = t('.notice')
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream_component_replace('tag_table_form', tags: @tags, target: @target, current_user: current_user),
-          turbo_stream_flash(flash)
-        ]
-      end
-      format.html { redirect_to root_path, alert: t('.alert'), status: :see_other }
-    end
-  end
+  #   @target.tag_list = params[:tag_list].split(',')
+  #   @target.save
+  #   @tags = @target.tag_counts_on(:tags)
+  #   flash[:notice] = t('.notice')
+  #   respond_to do |format|
+  #     format.turbo_stream do
+  #       render turbo_stream: [
+  #         turbo_stream_component_replace('tag_table_form', tags: @tags, target: @target, current_user: current_user),
+  #         turbo_stream_flash(flash)
+  #       ]
+  #     end
+  #     format.html { redirect_to root_path, alert: t('.alert'), status: :see_other }
+  #   end
+  # end
 
   private
 
