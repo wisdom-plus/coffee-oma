77dfa91d1fb659e047e34b02ab6e6c01f14c2b91
tomoron <tomoron326@outlook.jp>
2023-01-11 20:19:21 +0900

    fix:request tag spec

--- rails/spec/requests/tags_spec.rb
+++ rails/spec/requests/tags_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe 'Tags', type: :request do
 
   describe 'PATCH /tag' do
     it 'request success(product)' do
-      patch tag_path(product.id), params: { type: 'Product', tag_list: tag_list }
+      post tags_path(product.id, format: :turbo_stream, subaction: :refresh), params: { type: 'Product', tag_list: tag_list }
       expect(response).to have_http_status(:found)
     end
 
     it 'request success(bean)' do
-      patch tag_path(bean.id), params: { type: 'Bean', tag_list: tag_list }
+      post tags_path(bean.id, format: :turbo_stream, subaction: :refresh), params: { type: 'Bean', tag_list: tag_list }
       expect(response).to have_http_status(:found)
     end
   end
