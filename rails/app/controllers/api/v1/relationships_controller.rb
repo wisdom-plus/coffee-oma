abd0d19c523224f239ea6208ec58949f03d46d8a
tomoron <tomoron326@outlook.jp>
2023-02-08 20:08:43 +0900

    add:relationship controller

---
+++ rails/app/controllers/api/v1/relationships_controller.rb
@@ -0,0 +1,41 @@
+module Api
+  module V1
+    class RelationshipsController < ApiApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[create destroy]
+
+      def create
+        user = User.find_by(id: params[:relationships][:followed_id])
+        follow = current_api_v1_user.folow(user)
+        if follow.ni?
+          render status: :not_found
+        else
+          render status: :created
+        end
+      end
+
+      def destroy
+        follow = current_api_v1_user.unfollow(params[:id])
+        if follow.nil?
+          render status: :not_found
+        else
+          render status: :ok
+        end
+      end
+
+      def exists
+        user = User.find_by(id: params[:follow_id])
+        if user
+          follow =
+            if api_v1_user_signed_in?
+              current_api_v1_user.following?(user)
+            else
+              false
+            end
+          render json: { follow: follow }, status: :ok
+        else
+          render status: :not_found
+        end
+      end
+    end
+  end
+end
