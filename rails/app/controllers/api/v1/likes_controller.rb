3c92bd95796c55a5ae9e2e0ce406f06a5fc2da9e
tomoron <tomoron326@outlook.jp>
2023-02-10 20:48:56 +0900

    fix:likes_controller

--- rails/app/controllers/api/v1/likes_controller.rb
+++ rails/app/controllers/api/v1/likes_controller.rb
@@ -1,12 +1,12 @@
 module Api
   module V1
-    class LikesController < ApiApplicationController
+    class LikesController < ApplicationController
       before_action :authenticate_api_v1_user!, only: %i[create destroy]
 
       def index
         ranks = Product.all.order('likes_count desc').limit(9)
-        if likes.empty?
-          render status: :not_found, json: { message: 'いいねがありません' }
+        if ranks.empty?
+          render json: { message: 'いいねがありません' }, status: :not_found
         else
           render json: { likes: ranks }, status: :ok
         end
@@ -15,18 +15,18 @@ module Api
       def create
         like = current_api_v1_user.product_likes.new(like_params)
         if like.save
-          render status: :created
+          render json: {}, status: :created
         else
-          render status: :not_found
+          render json: {}, status: :not_found
         end
       end
 
       def destory
         like = current_api_v1_user.product_likes.find_by(product_id: params[:id])
         if like&.destory
-          render status: :ok
+          render json: {}, status: :ok
         else
-          render status: :not_found
+          render json: {}, status: :not_found
         end
       end
 
@@ -36,7 +36,7 @@ module Api
           liked = api_v1_user_signed_in && product.product_likes.exists(user_id: current_api_v1_user.id)
           render json: { liked: liked, count: product.likes.size }, status: :ok
         else
-          render status: :not_found
+          render json: {}, status: :not_found
         end
       end
 
