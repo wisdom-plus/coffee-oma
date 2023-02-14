ec674325618118597331553d4d154d4b922b127c
tomoron <tomoron326@outlook.jp>
2023-02-14 21:38:25 +0900

    fix:rspec config

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -103,6 +103,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Selenium::WebDriver.logger.level = :warn
+  Selenium::WebDriver.logger.ignore(:caps_browsers)
+
   config.include FactoryBot::Syntax::Methods
   config.include Warden::Test::Helpers
   config.include Devise::Test::IntegrationHelpers, type: :request
