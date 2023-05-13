868bf72a198881670de14694f971d643e7531b60
tomoron <tomoron326@outlook.jp>
2023-05-13 14:12:28 +0900

    add:controller action

--- rails/app/controllers/brands_controller.rb
+++ rails/app/controllers/brands_controller.rb
@@ -1,22 +1,41 @@
 class BrandsController < ApplicationController
   def index
+    @brands = Brand.all
   end
 
   def show
-  end
-
-  def create
+    @brand = Brand.find_by(id: params[:id])
   end
 
   def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(name: params[:name])
+    if @brand.save
+      redirect_to brands_path, notice: "Brand created successfully"
+    else
+      render :new
+    end
   end
 
   def edit
+    @brand = Brand.find_by(id: params[:id])
   end
 
   def update
+    @brand = Brand.find_by(id: params[:id])
+    if @brand.update(brand_params)
+      redirect_to brands_path, notice: "Brand updated successfully"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @brand = Brand.find_by(id: params[:id])
+    @brand.destroy
+    redirect_to brands_path, notice: "Brand deleted successfully"
   end
 end
