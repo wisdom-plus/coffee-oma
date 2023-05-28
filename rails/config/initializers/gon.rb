5994fb49efa79ac6d7d861725a76111523abdc05
tomoron <tomoron326@outlook.jp>
2023-05-28 09:08:18 +0900

    rm:gem gon

--- rails/config/initializers/gon.rb
+++
@@ -1,5 +0,0 @@
-Gon.global.tag_form_url = if Rails.env.production?
-                            ENV.fetch('TAG_URL', nil)
-                          else
-                            ENV.fetch('TAG_LOCAL_URL', nil)
-                          end
