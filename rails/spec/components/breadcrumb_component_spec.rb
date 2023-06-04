bc5f03152d4e7d5b2c5580ea0f51a790baff885a
tomoron <tomoron326@outlook.jp>
2023-06-04 09:28:32 +0900

    fix:breadcrumb component

--- rails/spec/components/breadcrumb_component_spec.rb
+++ rails/spec/components/breadcrumb_component_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe BreadcrumbComponent, type: :component do
+RSpec.describe Breadcrumb::Component, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
