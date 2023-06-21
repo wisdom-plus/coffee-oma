0ba41a0be1a64ebef398b1c9ab0de905b84247f9
tomoron <tomoron326@outlook.jp>
2023-06-21 09:16:49 +0900

    fix:user_registration_show

--- rails/config/breadcrumbs.rb
+++ rails/config/breadcrumbs.rb
@@ -83,7 +83,7 @@ crumb :user_edit do
 end
 
 crumb :profile do |user|
-  link 'プロフィール', users_registration_show_path(user.id)
+  link 'プロフィール', user_registration_show_path(user.id)
   parent :root
 end
 
