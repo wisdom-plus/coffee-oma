9f2cc318cca01e1f7605bdecd6ea72fd40647a09
tomoron <tomoron326@outlook.jp>
2023-06-30 09:01:04 +0900

    fix:registration rate

--- rails/spec/system/review_likes_spec.rb
+++ rails/spec/system/review_likes_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'ReviewLikes', js: true do
+RSpec.describe 'ReviewLikes' do
   let(:user) { create(:user) }
   let(:product) { create(:product, user: user) }
   let!(:review) { create(:review, user: user, product: product) }
