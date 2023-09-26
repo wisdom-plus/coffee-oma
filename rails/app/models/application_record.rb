1491359d1ed59043b011fc15a430ab1462dc2888
wisdom-plus <tomoron326@outlook.jp>
2023-09-27 08:43:01 +0900

    fix:ransackable

--- rails/app/models/application_record.rb
+++ rails/app/models/application_record.rb
@@ -4,4 +4,17 @@ class ApplicationRecord < ActiveRecord::Base
   INDEX_DISPALY_NUM = 9
   SHOW_DISPLAY_NUM = 5
   TOP_DISPALY_NUM = 3
+
+  class << self
+    def ransackable_attributes(auth_object = nil)
+      if Ransack::SUPPORTS_ATTRIBUTE_ALIAS
+        column_names + _ransackers.keys
+      else
+        column_names + _ransackers.keys + _ransackers.values.map(&:attribute_names).flatten
+      end.uniq
+    end
+    def ransackable_associations(auth_object = nil)
+      reflect_on_all_associations.map { |a| a.name.to_s }
+    end
+  end
 end
