7c98f62644a4984bf602f748db87631f628a511b
wisdom-plus <tomoron326@outlook.jp>
2024-05-22 08:10:46 +0900

    fix:mailer spec

--- rails/spec/mailers/contact_mailer_spec.rb
+++ rails/spec/mailers/contact_mailer_spec.rb
@@ -3,33 +3,33 @@ require 'rails_helper'
 RSpec.describe ContactMailer do
   let_it_be(:contact) { create(:contact) }
 
-  describe 'contact' do
-    context 'contact_mail' do
-      subject(:mail) do
-        ContactMailer.with(contact: contact).contact_mail.deliver_now
-        ActionMailer::Base.deliveries.last
-      end
-
-      it 'send mail' do
-        expect(mail.from.first).to eq('noreply@coffee-oma.com')
-        expect(mail.to.first).to eq(ENV.fetch('MY_MAILADRESS', nil))
-        expect(mail.subject).to eq('お問い合わせ通知')
-        expect(mail.body).to match('これは問合せメールの本文が入ります')
-      end
+  describe 'contact_mail' do
+    subject(:mail) do
+      mail = ContactMailer.with(contact: contact).contact_mail
+      Premailer::Rails::Hook.perform(mail)
     end
 
-    context 'reply_mail' do
-      subject(:mail) do
-        ContactMailer.with(contact: contact).reply_mail.deliver_now
-        ActionMailer::Base.deliveries.last
-      end
+    it 'send mail' do
+      expect(mail.from.first).to eq('noreply@coffee-oma.com')
+      expect(mail.to.first).to eq(ENV.fetch('MY_MAILADRESS', nil))
+      expect(mail.subject).to eq('お問い合わせ通知')
+      expect(mail.html_part.body.raw_source).to include('これは問合せメールの本文が入ります')
+      expect(mail.text_part.body.raw_source).to include('これは問合せメールの本文が入ります')
+    end
+  end
 
-      it 'send mail' do
-        expect(mail.from.first).to eq('noreply@coffee-oma.com')
-        expect(mail.to.first).to eq('test@example.com')
-        expect(mail.subject).to eq('お問い合わせありがとうございます')
-        expect(mail.body).to match('これは問合せメールの本文が入ります')
-      end
+  describe 'reply_mail' do
+    subject(:mail) do
+      ContactMailer.with(contact: contact).reply_mail.deliver_now
+      ActionMailer::Base.deliveries.last
+    end
+
+    it 'send mail' do
+      expect(mail.from.first).to eq('noreply@coffee-oma.com')
+      expect(mail.to.first).to eq('test@example.com')
+      expect(mail.subject).to eq('お問い合わせありがとうございます')
+      expect(mail.html_part.body.raw_source).to include('これは問合せメールの本文が入ります')
+      expect(mail.text_part.body.raw_source).to include('これは問合せメールの本文が入ります')
     end
   end
 end
