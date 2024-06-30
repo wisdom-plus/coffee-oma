9b8cdb88241aacee8b6b8edd77ff379d55e31ee6
wisdom-plus <tomoron326@outlook.jp>
2024-07-01 07:21:11 +0900

    like component spec

--- rails/spec/components/likes_user_show_component_spec.rb
+++ rails/spec/components/likes_user_show_component_spec.rb
@@ -3,13 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe LikesUserShow, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:product) { create(:product) }
+  let(:like) { create(:product_like, liked_id: product.id, user: user) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'renders a list of likes' do
+    like
+    likes = Like.find_product_and_bean(user)
+    render_inline(LikesUserShow::Component.new(likes: likes))
+    expect(page).to have_content(product.name)
+  end
+
+  it 'not render' do
+    render_inline(LikesUserShow::Component.new(likes: []))
+    expect(page).to have_content('まだお気に入りはありません')
+  end
 end
